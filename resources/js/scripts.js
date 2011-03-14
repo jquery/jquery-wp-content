080ea679abc85f60166ff07524d2f8750c4db311
Douglas Neiner <doug@pixelgraphics.us>
2011-03-14 13:42:19 -0400

    Changed from hard-tabs to 2 space soft-tabs on html, css, and js files for this project. Closes #1

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,80 +3,80 @@
 // Executes on DOM ready
 //
 App.subscribe("init", function(){
-	
-	//	
-	// Add Syntax Highlighting
-	//
-	SyntaxHighlighter.all();
-	
-	//
-	// Add Search Interactions
-	//
-	$("#search").inFieldLabels();
-	
-	
-	//
-	// Fancy Dropdown
-	//
-	$(".sdropdown").bind("mouseover", function(){
-		$(this).find("ul").stop().slideDown(200);
-	}).bind("mouseout", function(){
-		$(this).find("ul").stop().slideUp(200);
-	});
+  
+  //  
+  // Add Syntax Highlighting
+  //
+  SyntaxHighlighter.all();
+  
+  //
+  // Add Search Interactions
+  //
+  $("#search").inFieldLabels();
+  
+  
+  //
+  // Fancy Dropdown
+  //
+  $(".sdropdown").bind("mouseover", function(){
+    $(this).find("ul").stop().slideDown(200);
+  }).bind("mouseout", function(){
+    $(this).find("ul").stop().slideUp(200);
+  });
 
-	//
-	// Temporary: REMOVE
-	// Change page color
-	//
-	var colors = [ "jquery", "jquery-ui", "jquery-mobile", "jquery-project" ],
-		color_string = colors.join(' ');
-	$("ul.projects").delegate("li:lt(3)", "click", function(e) {
-		e.preventDefault();
-		$(document.documentElement)
-			.removeClass(color_string)
-			.addClass(this.className);
-		window.location.hash = this.className;
-	});
-	
-	if (window.location.hash && $.inArray(window.location.hash.substr(1), colors) > -1) {
-		$(document.documentElement)
-			.removeClass(color_string)
-			.addClass(window.location.hash.substr(1));
-	}
-	
-	//
-	// Project Select Show/Hide
-	//
-	$(".toggle-projects").bind("click", function(e){
-		e.preventDefault();
-		var $this = $(this).toggleClass("open");
-		
-		if ($this.hasClass("open")) {
-		    $("body > header").stop(true, false).animate({"marginTop":"140px"}, 500);
-		} else {
-		    $("body > header").stop(true, false).animate({"marginTop":"0"}, 500);
-		}
-		
-	});
+  //
+  // Temporary: REMOVE
+  // Change page color
+  //
+  var colors = [ "jquery", "jquery-ui", "jquery-mobile", "jquery-project" ],
+    color_string = colors.join(' ');
+  $("ul.projects").delegate("li:lt(3)", "click", function(e) {
+    e.preventDefault();
+    $(document.documentElement)
+      .removeClass(color_string)
+      .addClass(this.className);
+    window.location.hash = this.className;
+  });
+  
+  if (window.location.hash && $.inArray(window.location.hash.substr(1), colors) > -1) {
+    $(document.documentElement)
+      .removeClass(color_string)
+      .addClass(window.location.hash.substr(1));
+  }
+  
+  //
+  // Project Select Show/Hide
+  //
+  $(".toggle-projects").bind("click", function(e){
+    e.preventDefault();
+    var $this = $(this).toggleClass("open");
+    
+    if ($this.hasClass("open")) {
+        $("body > header").stop(true, false).animate({"marginTop":"140px"}, 500);
+    } else {
+        $("body > header").stop(true, false).animate({"marginTop":"0"}, 500);
+    }
+    
+  });
 
-	$(".presentations img, .books img").each(function (i, el) {
-		var $img = $(this),
-			$span = $img.parent();
-		
-		$span.css("background-image", "url(" + $img.attr('src') + ")");
-		$img.css("visibility", "hidden");
-	});
-	
-	$(".footer-icon-links")
-		.find("li a")
-			.append("<span></span>")
-			.end()
-		.delegate("li a", "mouseenter", function () {
-			$(this).find("span").stop(true, false).fadeTo(250, 1.0);
-		})
-		.delegate("li a", "mouseleave", function () {
-			$(this).find("span").stop(true, false).fadeOut(250);
-		});
+  $(".presentations img, .books img").each(function (i, el) {
+    var $img = $(this),
+      $span = $img.parent();
+    
+    $span.css("background-image", "url(" + $img.attr('src') + ")");
+    $img.css("visibility", "hidden");
+  });
+  
+  $(".footer-icon-links")
+    .find("li a")
+      .append("<span></span>")
+      .end()
+    .delegate("li a", "mouseenter", function () {
+      $(this).find("span").stop(true, false).fadeTo(250, 1.0);
+    })
+    .delegate("li a", "mouseleave", function () {
+      $(this).find("span").stop(true, false).fadeOut(250);
+    });
 });
 
 
