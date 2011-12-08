7644ae9aeccada9f7409384257f96fb8edc4d06d
Richard D. Worth <rdworth@gmail.com>
2011-12-08 14:18:42 +0000

    plugins theme: Added single.php and content-single.php

---
+++ themes/plugins-jquery-com/single.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="body" class="clearfix">
+			<div class="inner" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'single' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- .inner -->
+		</div><!-- #body -->
+
+<?php get_footer(); ?>
