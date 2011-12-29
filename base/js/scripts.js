5ec50b4bf75f29e8ee34f597d73fb800862ea933
adam j. sontag <ajpiano@ajpiano.com>
2011-12-29 17:55:10 -0500

    bring base javascript into parity with learning site progress, removing syntaxhighlighter and inieldlabels

--- base/js/scripts.js
+++ base/js/scripts.js
@@ -1,41 +1,128 @@
-jQuery.InFieldLabels.defaultOptions.defaultOpacity = 1;
-jQuery.InFieldLabels.defaultOptions.fadeOpacity    = 0.5;
 
+/*******************************************************************************/
+/*	Subscriptions */
+/*******************************************************************************/
 
+//
 // Executes on DOM ready
-jQuery(function() {
-  
-  var tooltip_timeout;
-  
-  // Add Syntax Highlighting
-  SyntaxHighlighter.all();
-  
-  // Add Search Interactions
-  $("#search").inFieldLabels();
-  
-  // Project Select Show/Hide
-  $(".toggle-projects").on( "click", function( e ) {
-    e.preventDefault();
+//
+App.subscribe("init", function(){
 
-    var el = $( this ).toggleClass("active");
+	//
+	// Set Auto Height
+	//
+	$(window).bind("load resize", function(){
+		$(".autoHeight").each(function(){
+			var el = $(this), parent = el.parent();
+			if(parent.height() >= el.height("auto").height()){
+				el.css({"margin-bottom":0,"padding-bottom":"25px","height":parent.height()});
+			} else {
+				el.css({"margin-bottom":0,"padding-bottom":"25px","height":el.height("auto").height()});
+			}
+		});
+    //		commented 12/29/11 unclear as to purpose
+//		$("#container #body").css({"overflow":"visible"});
+	});
+	
+	//
+	// Add Search Interactions
+	//
+	$("#search").bind('focus', function(){
+		$(this).parent().find("label").animate({opacity:'0.5'}, 200);
+	}).bind('blur', function(){
+		$(this).parent().find("label").animate({opacity:'1'}, 200);
+	}).bind('keypress', function(){
+		$(this).parent().find('label').hide();
+	}).bind('keyup', function(){
+		if($(this).val() == ''){
+			$(this).parent().find('label').show();
+		}
+	});
 
-    $("body").animate({ "marginTop": ( el.hasClass("active") ? "150px" : "0" ) }, 300, function() {
-      el.toggleClass("down");
-    });
-  });
-  
-  // Project Select Clickoutside
-  $(".project-select").on( "clickoutside", function( e ) {
-    var el = $(".toggle-projects");
-    if ( e.target.parentNode === el[0] || e.target === el[0] ) {
-      return;
-    }
+	//
+	// Project Select Show/Hide
+	//
+	$(".toggle-projects").bind("click", function(e){
+		e.preventDefault();
+		var el = $(this);
+		if(el.hasClass('active')){
+			el.removeClass('active');
+			$("body").css({"marginTop":"0"});
+			el.removeClass('down');
+		} else {
+			el.addClass('active');
+			$("body").css({"marginTop":"150px"});
+			el.addClass('down');
+		}
+	});
 
-    if ( el.hasClass("down") ) {
-      el.click();
-    }
-  });
-  
+	//
+	// Project Select Clickoutside
+	//
+	$(".project-select").bind("clickoutside", function(e, el){
+		var target = $(".toggle-projects");
+		if($(el).parent(".toggle-projects").length != 1){
+			if(target.hasClass('down')){
+				target.removeClass("active down");
+				$("body").css({"marginTop":"0"}, 300);
+			}
+		}
+	});
+
+	//
+	// Footer Books + Presentations
+	//
+	$(".presentations img, .books img").each(function (i, el) {
+		var $img = $(this),
+		$span = $img.parent();
+		$span.css("background-image", "url(" + $img.attr('src') + ")");
+		$img.css("visibility", "hidden");
+	});
+	
+	//
+	// Footer Social Icons
+	//
+	$(".footer-icon-links")
+		.find("li a")
+		.append("<span></span>")
+		.end()
+		.delegate("li a", "mouseenter", function () {
+			$(this).find("span").stop(true, false).fadeTo(250, 1.0);
+		})
+		.delegate("li a", "mouseleave", function () {
+			$(this).find("span").stop(true, false).fadeOut(250);
+		});
+		
+	//
+	// Run Van Gogh - Syntax Highlighting
+	//
+	$("pre").children("code").text(function(i, t) {
+		return $.trim( t );
+	}).parent().vanGogh();
+	
+	//
+	// Learning Site Specific
+	//
+	$("#sidebar .paper").bind("click", function(e){
+		// Allow the actual links inside to take the normal link behaviour
+		if ($(e.target).is("li a")) {
+			return;
+		}
+		e.preventDefault();
+		var el = $(this);
+		if(el.hasClass("open")){
+			el.removeClass("open").addClass("closed").animate({"width":"24%","margin-left":"-30.5%"}, 500);
+		} else {
+			el.removeClass("closed").addClass("open").animate({"width":"120%","margin-left":"-126.5%"}, 500);
+		}
+	});
+	$("#sidebar").bind("clickoutside", function(e){
+		var el = $(".paper",this);
+		if(el.hasClass("open")){
+			el.removeClass("open").addClass("closed").animate({"width":"24%","margin-left":"-30.5%"}, 500);
+		}
+	});
+	
   // Project Tooltips
   $(".projects").find(".jquery, .jquery-ui, .jquery-mobile").on( "mouseover", function() {
     var el = $( this ), 
@@ -77,67 +164,10 @@ jQuery(function() {
   }, function() {
     $( this ).children("ul").stop( true, true ).slideUp( 100 );
   });
-
-/*
-  // Temporary: REMOVE
-  // Change page color
-  var colors = [ "jquery", "jquery-ui", "jquery-mobile", "jquery-project" ],
-    color_string = colors.join(" ");
-  $("ul.projects").delegate("li:lt(3)", "click", function(e) {
-    e.preventDefault();
-    $(document.documentElement)
-      .removeClass(color_string)
-      .addClass(this.className);
-    window.location.hash = this.className;
-  });
-*/
-  
-  if ( window.location.hash && $.inArray( window.location.hash.substr( 1 ), colors ) > -1 ) {
-    $( document.documentElement )
-      .removeClass( color_string )
-      .addClass( window.location.hash.substr( 1 ) );
-  }
-
-  $(".presentations, .books").find("img").each(function ( i, el ) {
-    var $img = $( this ).css( "visibility", "hidden" );
-    $img.parent().css( "background-image", "url(" + $img.attr("src") + ")" );
-  });
-  
-  $(".footer-icon-links")
-    .find("li a")
-      .append("<span></span>")
-      .end()
-    .delegate( "li a", "mouseenter", function () {
-      $( this ).find("span").stop( true, false ).fadeTo( 250, 1.0 );
-    })
-    .delegate( "li a", "mouseleave", function () {
-      $( this ).find("span").stop( true, false ).fadeOut( 250 );
-    });
 	
-	autoHeight();
-	
-	$( window ).resize(autoHeight);
-	
-	function autoHeight() {
-		$(".autoHeight").each(function() {
-			var el = $( this ), 
-        parent = el.parent();
-
-			//console.log(el.height("auto").height());
-			//console.log(el.height("auto").height() + " : " + parent.height());
-			if ( parent.height() >= el.height("auto").height() ) {
-				el.css({ "height": parent.height() });
-			} 
-      else {
-				el.css({ "height": el.height("auto").height() });
-			}
-		});
-	}
-
   // CDN auto-select-all
   $('#site-footer .cdn input').click(function() {
     this.select && this.select();
   });
-	
-});
 
+});
