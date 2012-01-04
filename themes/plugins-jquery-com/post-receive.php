2756e756ddf4396621d5a3c11974a56ff32ea0f0
Scott González <scott.gonzalez@gmail.com>
2012-01-04 15:28:02 -0500

    plugins.jquery.com: Bail on GET requests for update.

--- themes/plugins-jquery-com/post-receive.php
+++ themes/plugins-jquery-com/post-receive.php
@@ -2,6 +2,10 @@
 /*
 Template Name: Post-Receive Hook
 */
+if ( $_SERVER["REQUEST_METHOD"] === "GET" ) {
+	// TODO: redirect to instructions for adding a plugin
+	exit;
+}
 $handle = popen( "node " . $post->post_content_filtered, "w" );
 fwrite( $handle, file_get_contents( "php://input" ) );
 pclose( $handle );
