10cba5c1f468fd993150caf3c8a2440b32a31717
Richard D. Worth <rdworth@gmail.com>
2012-01-05 20:08:19 +0000

    plugins: Removed unused article footer content, in particular the edit link

---
+++ themes/plugins-jquery-com/content-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+	<footer class="entry-meta">
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
