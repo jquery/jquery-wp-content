174cc004e60242d9a241e160b5fa03479c00224e
Karl Swedberg <kswedberg@gmail.com>
2012-06-11 08:44:02 -0400

    Remove non-php line from functions.php files

--- themes/api-jquery-com/functions.php
+++ themes/api-jquery-com/functions.php
@@ -1,9 +1,7 @@
 <?php
 // Common functions that can be used across all child themes:
 require_once get_template_directory() . '/functions.jquery.php';
-?>
 
-<?php
 // Child-theme-specific functions:
 
 ?>
