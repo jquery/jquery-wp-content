ef5ce108ce0572b06bfe5bf6ff75ed5d499a28f1
Richard D. Worth <rdworth@gmail.com>
2012-06-15 17:27:55 -0400

    jquerymobile-com theme: changed demos slug to docs in top-level menu

--- themes/jquerymobile-com/menu-header.php
+++ themes/jquerymobile-com/menu-header.php
@@ -1,6 +1,6 @@
 <div class="menu-top-container">
   <ul id="menu-top" class="menu">
-    <li class="menu-item"><a href="/demos">Docs</a></li>
+    <li class="menu-item"><a href="/docs">Docs</a></li>
     <li class="menu-item"><a href="/download">Download</a></li>
     <li class="menu-item"><a href="/gbs">Platforms</a></li>
     <li class="menu-item"><a href="/themeroller">Themes</a></li>
