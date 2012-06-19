1b826cf7d1105420987b825045dad25f337f7ed9
Richard D. Worth <rdworth@gmail.com>
2012-06-19 09:43:11 -0400

    jquery-com theme: Added Browser Support link to top nav

--- themes/jquery-com/menu-header.php
+++ themes/jquery-com/menu-header.php
@@ -3,5 +3,6 @@
     <li class="menu-item"><a href="http://plugins.jquery.com/">Plugins</a></li>
     <li class="menu-item"><a href="http://jquery.com/download">Download</a></li>
     <li class="menu-item"><a href="http://api.jquery.com/">API Documentation</a></li>
+    <li class="menu-item"><a href="http://jquery.com/browser-support">Browser Support</a></li>
   </ul>
 </div>
