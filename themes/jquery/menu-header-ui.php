9e3b45d4edb0bb0bd541511408dc7a959b6422c9
Richard D. Worth <rdworth@gmail.com>
2012-06-20 08:13:03 -0400

    ui themes: share top-nav and footer

---
+++ themes/jquery/menu-header-ui.php
@@ -0,0 +1,11 @@
+<div class="menu-top-container">
+  <ul id="menu-top" class="menu">
+    <li class="menu-item"><a href="http://jqueryui.com/demos/">Demos</a></li>
+    <li class="menu-item"><a href="http://jqueryui.com/download">Download</a></li>
+    <li class="menu-item"><a href="http://api.jqueryui.com/">API Documentation</a></li>
+    <li class="menu-item"><a href="http://jqueryui.com/themeroller">Themes</a></li>
+    <li class="menu-item"><a href="http://jqueryui.com/development">Development</a></li>
+    <li class="menu-item"><a href="http://jqueryui.com/support">Support</a></li>
+    <li class="menu-item"><a href="http://blog.jqueryui.com/">Blog</a></li>
+  </ul>
+</div>
