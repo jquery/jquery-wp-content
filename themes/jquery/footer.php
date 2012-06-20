2d08eba06885e2b897ae2b7b6e81e95b2407fbe8
Richard D. Worth <rdworth@gmail.com>
2012-06-20 07:53:31 -0400

    jquery theme: Corrected some footer links

--- themes/jquery/footer.php
+++ themes/jquery/footer.php
@@ -74,11 +74,11 @@
     </div>
     <div id="legal">
       <ul class="footer-site-links">
-        <li class="icon-learning-center icon"><a href="#">Learning Center</a></li>
+        <li class="icon-learning-center icon"><a href="http://learn.jquery.com/">Learning Center</a></li>
         <li class="icon-forum icon"><a href="http://forum.jquery.com/">Forum</a></li>
         <li class="icon-api icon"><a href="http://api.jquery.com/">API</a></li>
         <li class="icon-twitter icon"><a href="http://twitter.com/jquery">Twitter</a></li>
-        <li class="icon-irc icon"><a href="#">IRC</a></li>
+        <li class="icon-irc icon"><a href="http://irc.jquery.com/">IRC</a></li>
       </ul>
         <p class="copyright">Copyright <?php echo date('Y'); ?> <a href="http://jquery.org/team/">The jQuery Foundation</a>.<br /><span class="sponsor-line">Sponsored by <a href="http://mediatemple.net" class="mt-link">Media Temple</a> and <a href="http://jquery.org/sponsors/">others</a>.</span></p>
     </div>
