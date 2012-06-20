d9b91227471207dd94d6d3229f9b4bd130e38ebd
Richard D. Worth <rdworth@gmail.com>
2012-06-20 06:57:30 -0400

    jQuery themes: share top-nav menu

--- themes/plugins-jquery-com/menu-header.php
+++ themes/plugins-jquery-com/menu-header.php
@@ -1,7 +1,1 @@
-<div class="menu-top-container">
-  <ul id="menu-top" class="menu">
-    <li class="menu-item current-menu-item"><a href="http://plugins.jquery.com/">Plugins</a></li>
-    <li class="menu-item"><a href="http://jquery.com/download">Download</a></li>
-    <li class="menu-item"><a href="http://api.jquery.com/">API Documentation</a></li>
-  </ul>
-</div>
+<?php get_template_part('menuheader', 'jquery'); ?>
