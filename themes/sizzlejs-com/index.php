89db3a0b736f9cab2d19ad0e1207bb67a1de5f4b
Richard D. Worth <rdworth@gmail.com>
2012-06-25 08:04:47 -0400

    sizzlejs-com theme: remove index.php as the one in jquery/ works together with static index plugin

--- themes/sizzlejs-com/index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-	<?php /* Start the Loop */ ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-
-		<?php get_template_part( 'content' ); ?>
-
-	<?php endwhile; ?>
-<?php get_footer(); ?>
