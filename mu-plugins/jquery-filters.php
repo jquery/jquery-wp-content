f8695d8438faf96b4302d100d48908149fc933e9
Andrew Nacin <andrewnacin@gmail.com>
2012-06-25 14:28:16 -0400

    Allow full HTML in term descriptions. fixes #37.

--- mu-plugins/jquery-filters.php
+++ mu-plugins/jquery-filters.php
@@ -6,7 +6,7 @@
 // Disable smilies.
 add_filter( 'pre_option_use_smilies', '__return_zero' );
 
-// Turn on XML-RPC
+// Turn on XML-RPC.
 add_filter( 'pre_option_enable_xmlrpc', '__return_true' );
 
 // Disable WordPress auto-paragraphing for posts.
@@ -14,3 +14,13 @@ remove_filter( 'the_content', 'wpautop' );
 
 // Disable more restrictive multisite upload settings.
 remove_filter( 'upload_mimes', 'check_upload_mimes' );
+
+// Allow full HTML in term descriptions.
+add_action( 'init', 'jquery_unfiltered_html_for_term_descriptions' );
+add_action( 'set_current_user', 'jquery_unfiltered_html_for_term_descriptions' );
+function jquery_unfiltered_html_for_term_descriptions() {
+	remove_filter( 'pre_term_description', 'wp_filter_kses' );
+	remove_filter( 'pre_term_description', 'wp_filter_post_kses' );
+	if ( ! current_user_can( 'unfiltered_html' ) )
+		add_filter( 'pre_term_description', 'wp_filter_post_kses' );
+}
