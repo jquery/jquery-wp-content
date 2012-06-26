fb6c41b2b0903dffa738bdd68500e4552278dc54
Scott González <scott.gonzalez@gmail.com>
2012-06-25 22:29:59 -0400

    grunt-wordpress: Upgrade version to 1.0.1.

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -1,12 +1,12 @@
 <?php
 
-$live_domain = $_SERVER['HTTP_HOST'];
-if ( JQUERY_STAGING ) {
-	$live_domain = str_replace( JQUERY_STAGING_PREFIX, '', $live_domain );
-}
-if ( 0 === validate_file( $live_domain ) ) {
-	foreach ( (array) glob( dirname( __FILE__ ) . "/$live_domain/*.php" ) as $site_specific_file ) {
-		require_once( $site_specific_file );
-	}
-}
-unset( $live_domain, $site_specific_file );
+// $live_domain = $_SERVER['HTTP_HOST'];
+// if ( JQUERY_STAGING ) {
+// 	$live_domain = str_replace( JQUERY_STAGING_PREFIX, '', $live_domain );
+// }
+// if ( 0 === validate_file( $live_domain ) ) {
+// 	foreach ( (array) glob( dirname( __FILE__ ) . "/$live_domain/*.php" ) as $site_specific_file ) {
+// 		require_once( $site_specific_file );
+// 	}
+// }
+// unset( $live_domain, $site_specific_file );
