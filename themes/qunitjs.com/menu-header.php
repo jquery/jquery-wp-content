240a442026c8db062a406b01e8776da2c45f9232
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-06 12:06:37 -0700

    QUnit sites: Add link to menu-header for new page

--- themes/qunitjs.com/menu-header.php
+++ themes/qunitjs.com/menu-header.php
@@ -2,6 +2,7 @@
   <ul id="menu-top" class="menu">
     <li class="menu-item"><a href="http://qunitjs.com/">Home</a></li>
     <li class="menu-item"><a href="http://api.qunitjs.com/">API Documentation</a></li>
+    <li class="menu-item"><a href="http://qunitjs.com/intro">Intro to JS Unit Testing</a></li>
     <li class="menu-item"><a href="http://qunitjs.com/cookbook">Cookbook</a></li>
     <li class="menu-item"><a href="http://qunitjs.com/extensions">Extensions</a></li>
   </ul>
