aa8826a76e23bf7e66fbb93c84ac877b6c83adcf
Richa Avasthi <richa@avasthi.name>
2012-07-07 00:41:00 -0700

    Finish up sidebar styling.
    All that remains is the related plugins section.

--- themes/plugins.jquery.com/content-jquery_plugin.php
+++ themes/plugins.jquery.com/content-jquery_plugin.php
@@ -46,66 +46,68 @@
       </div> <!-- /.related-plugins -->
     </div> <!-- /.plugin-main -->
     <div class="plugin-metadata">
-      <div class="block clearfix">
-        <div class="col col4-2">
-          <h2>Version</h2>
-          <p><b><?php echo jq_release_version(); ?></b></p>
-        </div>
-        <div class="col col4-2 right">
-          <h2>Released</h2>
-          <p><b><?php echo jq_release_date(); ?></b></p>
-        </div>
-      </div>
-      <div class="block">
-        <h2>Get it now</h2>
-        <ul>
-          <li><a href="<?php echo jq_release_download_url(); ?>"><b>Download</b> plugin</a></li>
-          <li><a href="<?php echo jq_plugin_repo_url(); ?>"><b>Fork</b> on GitHub</a></li>
-          <?php if ( jq_release_homepage() ) { ?>
-          <li><a href="<?php echo jq_release_homepage(); ?>"><b>View</b> homepage</a></li>
-          <?php } ?>
-          <?php if ( jq_release_demo() ) { ?>
-            <li><a href="<?php echo jq_release_demo(); ?>"><b>Try</b> a demo</a></li>
-          <?php } ?>
-          <?php if ( jq_release_docs() ) { ?>
-            <li><a href="<?php echo jq_release_docs(); ?>"><b>Read</b> the docs</a></li>
-          <?php } ?>
-        </ul>
-      </div>
-      <div class="block clearfix">
-        <div class="col col4-2">
-          <h2>Watchers</h2>
-          <p><b><?php echo jq_plugin_watchers(); ?></b></p>
-        </div>
-        <div class="col col4-2 right">
-          <h2>Forks</h2>
-          <p><b><?php echo jq_plugin_forks(); ?></b></p>
-        </div>
-      </div>
-      <div class="block">
+      <div class="block toolbox">
+        <div class="inner">
+          <header class="clearfix">
+            <div class="version-info">
+              <p class="caption">Version</p>
+              <p class="version-number"><?php echo jq_release_version(); ?></p>
+            </div> <!-- /.version-info -->
+            <div class="release-info">
+              <p class="caption">Released</p>
+              <p class="date"><?php echo jq_release_date(); ?></p>
+            </div>
+          </header>
+          <div class="body">
+            <a class="download" href="<?php echo jq_release_download_url(); ?>">
+              <span class="inner-wrapper">Download now</span>
+            </a>
+            <a class="other-link gh-fork" href="<?php echo jq_plugin_repo_url(); ?>">Fork on GitHub</a>
+            <a class="other-link view-homepage" href="<?php echo jq_release_homepage(); ?>">View homepage</a>
+            <a class="other-link demo" href="<?php echo jq_release_demo(); ?>">Try a demo</a>
+            <a class="other-link read-docs" href="<?php echo jq_release_docs(); ?>">Read the docs</a>
+          </div> <!-- /.body -->
+        </div> <!-- /.inner -->
+      </div> <!-- /.toolbox -->
+      <div class="block github-activity group">
+        <h2>GitHub Activity</h2>
+        <div class="box">
+          <div class="info-block watchers">
+            <p class="number"><?php echo jq_plugin_watchers(); ?></p>
+            <p class="caption">Watchers</p>
+          </div> <!-- /.watchers -->
+          <div class="info-block forks">
+            <p class="number"><?php echo jq_plugin_forks(); ?></p>
+            <p class="caption">Forks</p>
+          </div> <!-- /.forks -->
+        </div> <!-- /.box -->
+      </div> <!-- /.github-activity -->
+      <div class="block author-info">
         <h2>Author</h2>
-        <?php echo jq_release_author(); ?>
-      </div>
+        <ul>
+          <li><?php echo jq_release_author(); ?></li>
+        </ul>
+      </div> <!-- /.author-info -->
       <?php if ( $maintainers = jq_release_maintainers() ) { ?>
-      <div class="block">
+      <div class="block maintainer-info">
         <h2>Maintainers</h2>
         <ul>
           <?php echo $maintainers; ?>
         </ul>
-      </div><!-- .block -->
+      </div> <!-- /.maintainer-info -->
       <?php } ?>
-      <div class="block">
+      <div class="block licenses">
         <h2>Licenses</h2>
         <ul>
-        <?php echo jq_release_licenses(); ?>
+          <?php echo jq_release_licenses(); ?>
         </ul>
-      </div>
-      <div class="block">
+      </div> <!-- /.licenses -->
+      <div class="block dependencies">
         <h2>Dependencies</h2>
         <ul>
-        <?php echo jq_release_dependencies(); ?>
+          <?php echo jq_release_dependencies(); ?>
         </ul>
-      </div>
+      </div> <!-- /.dependencies -->
     </div> <!-- /.plugin-metadata -->
   </div><!-- .entry-content -->
   <footer class="entry-meta">
