d46cfbcb032cefbc90d7ed80a7c4b1808e3a7816
Jonathan Longnecker <jon@fortysevenmedia.com>
2012-07-26 17:37:57 -0400

    Working on Global Banner Styles. Added Journal font for use in all sites.

--- themes/plugins.jquery.com/index.php
+++ themes/plugins.jquery.com/index.php
@@ -6,6 +6,14 @@
 		<!-- inner -->
 		<div class="inner">
 
+<div id="banner">
+	<h1>Plugins Make jQuery More Awesomer</h1>
+	<h2>Level up your project, not your grammar</h2>
+	
+	<?php get_search_form(); ?>
+	
+</div>
+
 <?php
 
 // TODO
@@ -24,11 +32,30 @@ foreach( $toplvlpages as $post ) {
 ?>
 
 		</div> <!-- /.inner -->
-		<?php if($sidebar): ?>
-
-			<?php get_sidebar( ); ?>
-
-		<?php endif; ?>
+		 <h3>New Plugins</h3>
+		  <?php jq_new_plugins(); ?>
+		  <h3>Recent Updates</h3>
+		  <?php jq_updated_plugins(); ?>
+		
+		  <h3>Popular Tags</h3>
+		  <ul>
+		  <?php
+		  $tags_args = array('orderby' => 'count', 'order' => 'DESC', 'number' => 10);
+		  $tags = get_tags( $tags_args );
+		  $tag_html = '';
+		  foreach ($tags as $tag){
+		    $tag_link = get_tag_link($tag->term_id);
+		    $tag_html .= '<li>';
+		    $tag_html .= '<a href="' . $tag_link . '">';
+		      $tag_html .= $tag->name;
+		    $tag_html .= '</a>';
+		    $tag_html .=  $tag->count;
+		    $tag_html .= '</li>';
+		  }
+		
+		  echo $tag_html;
+		  ?>
+		  </ul>
 
 	</div>
 	<!-- /body -->
