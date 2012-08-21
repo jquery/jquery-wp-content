c473ebfac8fbae529861c5251124ebb7eb886e40
Scott González <scott.gonzalez@gmail.com>
2012-08-21 17:46:39 -0400

    api.qunitjs.com: Provide a short overview on the index page.

--- themes/api.qunitjs.com/index.php
+++ themes/api.qunitjs.com/index.php
@@ -6,6 +6,35 @@
 get_header();
 ?>
 
-<?php get_template_part('index', 'api'); ?>
+<!-- body -->
+<?php global $sidebar; ?>
+<div id="body" class="clearfix <?php echo $sidebar; ?>">
+	<div class="inner" role="main">
+		<h1>QUnit API Documentation</h1>
+		<hr class="dots">
+
+		<p>QUnit is a powerful, easy-to-use JavaScript unit test suite.
+			If you're new to QUnit or even just new to unit testing, you might
+			want to check out the <a href="http://qunitjs.com/intro/">Introduction
+			to JavaScript Unit Testing</a>. There's also a
+			<a href="http://qunitjs.com/cookbook/">QUnit Cookbook</a> on the
+			<a href="http://qunitjs.com/">main site</a>.</p>
+
+		<p>To get started, use the search at the top of the page, view the
+			<a href="/category/All/">full listing of entries</a>, or browse by
+			category:</p>
+
+		<ul>
+		<?php wp_list_categories( array( 'title_li' => null ) ); ?>
+		</ul>
+
+		<p>QUnit has no dependencies and works in all browsers.</p>
+	</div><!-- .inner -->
+
+	<?php if($sidebar): ?>
+		<?php get_sidebar( 'api' ); ?>
+	<?php endif; ?>
+
+</div><!-- #body -->
 
 <?php get_footer(); ?>
