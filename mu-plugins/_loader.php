c8a342c8bc2f5bb582f95015e8e5019446687cd9
Andrew Nacin <andrewnacin@gmail.com>
2012-08-30 11:27:48 -0400

    Add new directory for mu-plugins of a particular type of site, based on {$subdomain}-sites. Example: blog-sites or api-sites.

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -8,5 +8,11 @@ if ( 0 === validate_file( $live_domain ) ) {
 	foreach ( (array) glob( dirname( __FILE__ ) . "/$live_domain/*.php" ) as $site_specific_file ) {
 		require_once( $site_specific_file );
 	}
+	list( $subdomain ) = $live_domain = explode( '.', $live_domain );
+	if ( count( $live_domain ) > 2 ) {
+		foreach ( (array) glob( dirname( __FILE__ ) . "/$subdomain-sites/*.php" ) as $type_specific_file ) {
+			require_once( $type_specific_file );
+		}
+	}
 }
-unset( $live_domain, $site_specific_file );
+unset( $live_domain, $subdomain, $site_specific_file, $type_specific_file );
