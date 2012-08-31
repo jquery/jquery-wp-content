2dca2fbd3e70d2a48df40f97fa6e3aa9bf7b23fa
Timo Tijhof <krinklemail@gmail.com>
2012-08-31 06:23:45 +0300

    qunitjs.com: Avoid hardcoding as much as possible
    Follows-up eafdc2c382aaaf8dedac5bc474dbb032478a40cc.
    Thanks Scott!

--- themes/qunitjs.com/menu-header.php
+++ themes/qunitjs.com/menu-header.php
@@ -1,9 +1,9 @@
 <div class="menu-top-container">
   <ul id="menu-top" class="menu">
-    <li class="menu-item"><a href="http://qunitjs.com/">Home</a></li>
-    <li class="menu-item"><a href="http://api.qunitjs.com/">API Documentation</a></li>
-    <li class="menu-item"><a href="http://qunitjs.com/intro/">Intro to JS Unit Testing</a></li>
-    <li class="menu-item"><a href="http://qunitjs.com/cookbook/">Cookbook</a></li>
-    <li class="menu-item"><a href="http://qunitjs.com/extensions/">Extensions</a></li>
+    <li class="menu-item"><a href="/">Home</a></li>
+    <li class="menu-item"><a href="//api.qunitjs.com/">API Documentation</a></li>
+    <li class="menu-item"><a href="/intro/">Intro to JS Unit Testing</a></li>
+    <li class="menu-item"><a href="/cookbook/">Cookbook</a></li>
+    <li class="menu-item"><a href="/extensions/">Extensions</a></li>
   </ul>
 </div>
