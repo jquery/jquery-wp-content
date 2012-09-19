fed233cfe9719a975c87fc6f770b37aa00661742
Scott González <scott.gonzalez@gmail.com>
2012-09-19 10:34:00 -0400

    jqueryui.com: Started index page.

--- themes/jqueryui.com/index.php
+++ themes/jqueryui.com/index.php
@@ -1,7 +1,38 @@
 <?php get_header(); ?>
 
-<div id="body" class="clearfix">
-	<div class="inner"></div>
+<div id="body" class="clearfix sidebar-left">
+	<div class="inner">
+
+		<div id="banner"><div class="glow clearfix">
+			<p class="intro">jQuery UI is a curated set of user interface interactions,
+			effects, widgets, and themes built on top of the jQuery JavaScript Library.
+			Whether you're building highly interactive web applications or you just
+			need to add a date picker to a form control, jQuery UI is the perfect
+			choice.</p>
+
+			<div class="download-box">
+				<h2>Download jQuery UI 1.9.0</h2>
+				<a href="/download/" class="btn">Custom Download</a>
+				<p>Quick Downloads:</p>
+				<div class="download-option">
+					<a href="#" class="btn">Stable</a>
+					<span>v1.9.0</span>
+					<span>jQuery 1.6+</span>
+				</div>
+				<div class="download-option download-legacy">
+					<a href="#" class="btn">Legacy</a>
+					<span>v1.8.23</span>
+					<span>jQuery 1.3.2+</span>
+				</div>
+			</div>
+		</div></div>
+
+		<div class="latest-news">
+			<p><span>Latest News:</span> jQuery UI 1.9.0 released with hundreds of bug fixes and three new widgets. <a href="http://blog.jqueryui.com/">read more...</a></p>
+		</div>
+	</div>
+
+	<?php get_sidebar(); ?>
 </div>
 
 <?php get_footer(); ?>
