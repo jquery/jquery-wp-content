9d48582254014e07bf8f08bf65298a2d07da7b5e
Scott González <scott.gonzalez@gmail.com>
2012-09-20 12:37:38 -0400

    jqueryui.com: Moved index page into content repo.

--- themes/jquery/content-page.php
+++ themes/jquery/content-page.php
@@ -5,9 +5,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
+	<?php if ( !count( get_post_meta( $post->ID, "hide_title" ) ) ) : ?>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
