96b8b767171850328442868ac038467fc7341c28
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-09-26 19:26:52 +0200

    Fix regression in loader, renaming the other two instances of the var

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -3,7 +3,7 @@
 list( $live_domain ) = explode( '/', JQUERY_LIVE_SITE, 2 );
 if ( 0 === validate_file( $live_domain ) ) {
 	foreach ( (array) glob( dirname( __FILE__ ) . "/$live_domain/*.php" ) as $domain_specific_file ) {
-		require_once( $site_specific_file );
+		require_once( $domain_specific_file );
 	}
 	list( $subdomain ) = $live_domain = explode( '.', $live_domain );
 	if ( count( $live_domain ) > 2 ) {
@@ -12,4 +12,4 @@ if ( 0 === validate_file( $live_domain ) ) {
 		}
 	}
 }
-unset( $live_domain, $subdomain, $site_specific_file, $type_specific_file );
+unset( $live_domain, $subdomain, $domain_specific_file, $type_specific_file );
