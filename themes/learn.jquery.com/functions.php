7e32b8c220d09b24cc2e96ce2d80848df259149a
John Paul <john@johnkpaul.com>
2012-10-15 16:17:59 -0400

    add next and previous buttons to learn pages (cherry picked from commit c07b3632e83757591ddcfe8d96c58594d9a48829)

--- themes/learn.jquery.com/functions.php
+++ themes/learn.jquery.com/functions.php
@@ -10,3 +10,35 @@ function is_subpage() {
         return false;                          // ... the answer to the question is false
     }
 }
+
+if ( version_compare( $wp_version, '3.5-alpha', '<' ) ) :
+    add_filter( 'posts_where_paged', function( $where, $query ) {
+        global $wpdb;
+        if ( $menu_order = absint( $query->get( 'menu_order' ) ) )
+            $where .= " AND $wpdb->posts.menu_order = " . $menu_order;
+        return $where;
+    }, 10, 2 );
+endif;
+
+function get_next_prev_post() {
+    global $post;
+    $menu_order_prev = $post->menu_order - 1;
+    $menu_order_next = $post->menu_order + 1;
+
+    $posts_prev= new WP_Query( array(
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'menu_order' => $menu_order_prev
+    ) );
+
+    $posts_next= new WP_Query( array(
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'menu_order' => $menu_order_next
+    ) );
+
+    return array(
+        'prev' => count($posts_prev->posts) ? $posts_prev->posts[0] : NULL,
+        'next' => count($posts_next->posts) ? $posts_next->posts[0] : NULL
+    );
+}
