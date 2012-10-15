c07b3632e83757591ddcfe8d96c58594d9a48829
John Paul <john@johnkpaul.com>
2012-10-15 16:17:59 -0400

    add next and previous buttons to learn pages

--- themes/learn.jquery.com/page.php
+++ themes/learn.jquery.com/page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-		
+<?php $next_prev_arr = get_next_prev_post(); ?>
+
   <!-- body -->
   <?php global $sidebar; ?>
   <div id="body" class="clearfix <?php echo $sidebar; ?>">
@@ -14,6 +15,25 @@
 				<h2>Suggestions, Problems, Feedback</h2>
 				<a class="btn" href="<?php echo jq_get_github_url(); ?>"><i class="icon-github"></i>  Open an Issue or Submit a Pull Request on GitHub</a>
 
+
+        <div class='bottom-links'>
+            <?php if (isset($next_prev_arr['prev'])): ?>
+                   <div class='prev'>
+                       <a href="<?php echo $next_prev_arr['prev']->guid; ?>">
+                           <i class="icon-chevron-left"></i>
+                           <?php echo $next_prev_arr['prev']->post_title; ?>
+                       </a>
+                   </div>
+            <?php endif; ?>
+            <?php if (isset($next_prev_arr['next'])): ?>
+                   <div class='next'>
+                       <a href="<?php echo $next_prev_arr['next']->guid; ?>">
+                           <?php echo $next_prev_arr['next']->post_title; ?>
+                           <i class="icon-chevron-right"></i>
+                       </a>
+                   </div>
+            <?php endif; ?>
+        </div>
     </div>
     <!-- /inner -->
     
