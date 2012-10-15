780d8f0afa49b8f8513325a9c7d713d84931d7b3
John Paul <john@johnkpaul.com>
2012-10-15 16:25:35 -0400

    add suggestions link to base template

--- themes/jquery/page.php
+++ themes/jquery/page.php
@@ -19,6 +19,9 @@ get_header(); ?>
 
 				<?php comments_template( '', true ); ?>
 
+				<h2>Suggestions, Problems, Feedback</h2>
+				<a class="btn" href="<?php echo jq_get_github_url(); ?>"><i class="icon-github"></i>  Open an Issue or Submit a Pull Request on GitHub</a>
+
 			</div><!-- .inner -->
 		</div><!-- #body -->
 
