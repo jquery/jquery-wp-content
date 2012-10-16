348c33c85b30d3c24a8043bdea9c844810abe1c0
David Schnur <dnschnur@gmail.com>
2012-10-16 11:20:17 -0400

    Initial migration of plugins site to new-new-css.
    Mostly straightforward migration to the new-new CSS framework,
    cross-referencing the new foundation site with the old stage.jquery.com.
    
    Major changes include:
    - Split the sidebar from content-jquery_plugin.php off into its own
    include, sidebar-jquery_plugin.php
    - Removed several no-longer-necessary wrapper divs
    - Cleaned-up style.css organization; it had gotten messy
    
    Remaining issues:
    - Single-plugin page project info is still broken
    - Lots of pruning still required to remove styles that are now in base
    - Media queries haven't yet been migrated

--- themes/plugins.jquery.com/search.php
+++ themes/plugins.jquery.com/search.php
@@ -9,44 +9,41 @@
 
 get_header(); ?>
 
-<section id="body" class="clearfix">
-	<div class="inner clearfix" role="main">
-		<div id="content">
+<div id="content">
 
-			<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyeleven' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header>
+		<header class="page-header">
+			<h1 class="page-title"><?php printf( __("Search Results for '%s'", "twentyeleven"), "<span>" . get_search_query() . "</span>" ); ?></h1>
+		</header>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content-listing', get_post_format() ); ?>
-				<?php endwhile; ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content-listing', get_post_format() ); ?>
+		<?php endwhile; ?>
 
-				<div class="nav-previous">
-					<?php next_posts_link( '&larr; Older plugins' ); ?>
-				</div>
-				<div class="nav-next">
-					<?php previous_posts_link( 'Newer plugins &rarr;' ); ?>
-				</div>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p>Sorry, but nothing matched your search criteria.</p>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
+		<div class="nav-previous">
+			<?php next_posts_link( '&larr; Older plugins' ); ?>
 		</div>
-		<?php get_sidebar(); ?>
-	</div><!-- .inner -->
-</section><!-- #body -->
+		<div class="nav-next">
+			<?php previous_posts_link( 'Newer plugins &rarr;' ); ?>
+		</div>
+
+	<?php else : ?>
+
+		<article id="post-0" class="post no-results not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<p>Sorry, but nothing matched your search criteria.</p>
+			</div><!-- .entry-content -->
+		</article><!-- #post-0 -->
+
+	<?php endif; ?>
+
+</div><!-- #content -->
+
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- themes/plugins.jquery.com/single-jquery_plugin.php
+++ themes/plugins.jquery.com/single-jquery_plugin.php
@@ -14,24 +14,24 @@
 
 get_header(); ?>
 
-		<div id="body" class="clearfix">
-			<div class="inner" role="main">
-				<?php the_post(); ?>
-				<?php if ( $post->post_parent && (jq_release_version() !== jq_release_version( $post->post_parent )) ) : ?>
-					<div class="notify">
-						<p><i class="icon-warning-sign"></i>
-						<?php if ( jq_release_is_stable() ) : ?>
-							<span>This version is old school, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest version</a>.</span>
-						<?php else : ?>
-							<span>This is a pre-release version, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest stable version</a>.</span>
-						<?php endif; ?>
-						</p>
-					</div>
-				<?php endif; ?>
+<div id="content" class="jquery-plugin">
+	<?php the_post(); ?>
+	<?php if ( $post->post_parent && (jq_release_version() !== jq_release_version( $post->post_parent )) ) : ?>
+		<div class="notify">
+			<p><i class="icon-warning-sign"></i>
+			<?php if ( jq_release_is_stable() ) : ?>
+				<span>This version is old school, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest version</a>.</span>
+			<?php else : ?>
+				<span>This is a pre-release version, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest stable version</a>.</span>
+			<?php endif; ?>
+			</p>
+		</div>
+	<?php endif; ?>
 
-				<?php get_template_part( 'content', 'jquery_plugin' ); ?>
+	<?php get_template_part( 'content', 'jquery_plugin' ); ?>
 
-			</div><!-- .inner -->
-		</div><!-- #body -->
+</div><!-- #content -->
+
+<?php get_template_part( 'sidebar', 'jquery_plugin' ); ?>
 
 <?php get_footer(); ?>
