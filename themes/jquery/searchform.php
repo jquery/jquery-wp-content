056a735943212fad2836554f4b76e178d7224760
Tony Stuck <toekneestuck@gmail.com>
2012-10-16 10:47:08 -0400

    Removed inline script from search input. Cleaned up main.js file and removed nested jQuery.ready calls

--- themes/jquery/searchform.php
+++ themes/jquery/searchform.php
@@ -5,5 +5,5 @@
 ?>
 <form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<i class="icon-search"></i>
-	<input type="text" name="s" onclick="this.value='Search jQuery'?'':this.value;" onfocus="this.select()" onblur="this.value=!this.value?'Search jQuery':this.value;" value="Search jQuery" />
+	<input type="text" name="s" value="Search jQuery" />
 </form>
