d9c52da8020a544882121aadd8fea4e4328275a4
David Schnur <dnschnur@gmail.com>
2012-10-16 13:14:04 -0400

    Migrated plugin tags and search to new-new-css.
    Fixed plugin title and info styles, plus various minor fixes.

--- themes/plugins.jquery.com/search.php
+++ themes/plugins.jquery.com/search.php
@@ -14,7 +14,7 @@ get_header(); ?>
 	<?php if ( have_posts() ) : ?>
 
 		<header class="page-header">
-			<h1 class="page-title"><?php printf( __("Search Results for '%s'", "twentyeleven"), "<span>" . get_search_query() . "</span>" ); ?></h1>
+			<h1 class="page-title"><?php printf( __("Search Results for '%s'", 'twentyeleven'), '<span>' . get_search_query() . '</span>' ); ?></h1>
 		</header>
 
 		<?php while ( have_posts() ) : the_post(); ?>
