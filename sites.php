4cbf4b6c0b38371eb2a4b98abcbf1ccdc735923a
Scott González <scott.gonzalez@gmail.com>
2012-11-03 12:07:26 -0400

    api.jquerymobile.com: Use static index. (cherry picked from commit e756deacec3acea9a38a919caf2471ac8163d566)

--- sites.php
+++ sites.php
@@ -159,6 +159,9 @@ function jquery_sites() {
 			'options' => array(
 				'blogname' => 'jQuery Mobile API Documentation',
 				'stylesheet' => 'api.jquerymobile.com',
+				'active_plugins' => array(
+					'jquery-static-index.php',
+				),
 			),
 		),
 		'api.qunitjs.com' => array(
