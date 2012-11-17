a16b45fd426457919919ac29e78a15f6e4b34392
Richard D. Worth <rdworth@gmail.com>
2012-11-17 14:15:44 -0500

    Initial set of changes for jqueryui.com theme in new-css branch

--- themes/jqueryui.com/page.php
+++ themes/jqueryui.com/page.php
@@ -10,17 +10,15 @@
 
 get_header(); ?>
 
-<div id="body" class="sidebar-left clearfix">
-	<div class="inner" role="main">
+<div id="content" class="sidebar-left clearfix">
 
-		<?php the_post(); ?>
+	<?php the_post(); ?>
 
-		<?php get_template_part( 'content', 'page' ); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
 
-		<?php comments_template( '', true ); ?>
+	<?php comments_template( '', true ); ?>
 
-	</div>
-	<?php get_sidebar(); ?>
 </div>
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- themes/jqueryui.com/single.php
+++ themes/jqueryui.com/single.php
@@ -1,14 +1,11 @@
 <?php get_header(); ?>
 
-<div id="body" class="sidebar-left clearfix">
-	<div class="inner" role="main">
+<div id="content" class="sidebar-left clearfix">
+	<?php the_post(); ?>
 
-		<?php the_post(); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
 
-		<?php get_template_part( 'content', 'page' ); ?>
-
-	</div>
-	<?php get_sidebar(); ?>
 </div>
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
