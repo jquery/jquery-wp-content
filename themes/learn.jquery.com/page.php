45cd6921706d8e40bd98862e47d09871f9948dd3
adam j. sontag <ajpiano@ajpiano.com>
2012-11-19 19:12:51 -0500

    Add post modified date/time to learn site footer, rearrange learn site footer

--- themes/learn.jquery.com/page.php
+++ themes/learn.jquery.com/page.php
@@ -11,30 +11,6 @@
 				<?php the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php comments_template( '', true ); ?>
-				<h2>Suggestions, Problems, Feedback</h2>
-				<a class="btn" href="<?php echo jq_get_github_url(); ?>"><i class="icon-github"></i>  Open an Issue or Submit a Pull Request on GitHub</a>
-
-
-        <div class='bottom-links'>
-            <?php if (isset($next_prev_arr['prev'])): ?>
-                   <div class='prev'>
-                       <a href="<?php echo $next_prev_arr['prev']->guid; ?>">
-                           <i class="icon-chevron-left"></i>
-                           <?php echo $next_prev_arr['prev']->post_title; ?>
-                       </a>
-                   </div>
-            <?php endif; ?>
-            <?php if (isset($next_prev_arr['next'])): ?>
-                   <div class='next'>
-                       <a href="<?php echo $next_prev_arr['next']->guid; ?>">
-                           <?php echo $next_prev_arr['next']->post_title; ?>
-                           <i class="icon-chevron-right"></i>
-                       </a>
-                   </div>
-            <?php endif; ?>
-        </div>
     </div>
     <!-- /inner -->
     
