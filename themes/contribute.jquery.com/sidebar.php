0810850c94dd24027d1183bd38c5254a905643da
adam j. sontag <ajpiano@ajpiano.com>
2012-12-13 14:11:14 -0500

    contribute.jquery.com: Adds a basic child theme for contribute.jquery.com. Fixes #139.

---
+++ themes/contribute.jquery.com/sidebar.php
@@ -0,0 +1,14 @@
+<div id="sidebar" class="widget-area" role="complementary">
+	<aside class="widget">
+		<h3 class="widget-title">Contributing to &hellip;</h3>
+		<ul>
+			<li><a href="/triage/">Bug Triage</a><li>
+			<li><a href="/code/">Code</a><li>
+			<li><a href="/community/">Community</a><li>
+			<li><a href="/documentation/">Documentation</a><li>
+			<li><a href="/open-source/">Open Source</a><li>
+			<li><a href="/support/">Support</a><li>
+			<li><a href="/web/">Web Sites</a><li>
+		</ul>
+	</aside>
+</div>
