1ab3fa3ea0deba8e3704abf3d5dda9cf53315ff0
Richard D. Worth <rdworth@gmail.com>
2012-12-13 22:23:11 -0500

    Added support for pages on jquery.com theme to have #jq-primaryContent div wrapper div added by theme

--- themes/jquery.com/content-page.php
+++ themes/jquery.com/content-page.php
@@ -3,5 +3,16 @@
  * The template used for displaying page content in page.php
  */
 ?><div id="jq-content" class="jq-clearfix">
-<?php the_content(); ?>
+<?php
+$jqpc = get_post_meta( $post->ID, "jq-primaryContent" );
+if ($jqpc[0]) { ?>
+	<style>
+	#jq-primaryContent ul ul { margin-left: 1.5em; }
+	</style>
+	<div id="jq-primaryContent" style="width:auto;">
+		<?php the_content(); ?>
+	</div>
+<?php } else { ?>
+	<?php the_content(); ?>
+<?php } ?>
 </div><!-- /#content -->
