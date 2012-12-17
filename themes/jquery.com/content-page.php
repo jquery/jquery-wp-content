df2abbb348b0af799224506faba01f27f31260b4
Dave Methvin <dave.methvin@gmail.com>
2012-12-16 21:29:30 -0500

    Small change to force a deploy

--- themes/jquery.com/content-page.php
+++ themes/jquery.com/content-page.php
@@ -8,7 +8,7 @@ $jqpc = get_post_meta( $post->ID, "jq-primaryContent" );
 if ($jqpc[0]) { ?>
 	<style>
 	#jq-primaryContent ul ul { margin-left: 1.5em; }
-	.visuallyhidden { display: none; }
+	.visuallyhidden  { display: none; }
 	</style>
 	<div id="jq-primaryContent" style="width:auto;">
 		<?php the_content(); ?>
