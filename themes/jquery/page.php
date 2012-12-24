ca478e69b67a8827fea637187a52beae0a21943f
Scott González <scott.gonzalez@gmail.com>
2012-12-24 14:52:12 -0500

    Update default page template.

--- themes/jquery/page.php
+++ themes/jquery/page.php
@@ -1,21 +1,17 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<?php the_post(); ?>
 
+<div class="content-right">
+	<div id="content">
+		<?php if ( !count( get_post_meta( $post->ID, "hide_title" ) ) ) : ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<hr>
+		<?php endif; ?>
 
-				<?php the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php comments_template( '', true ); ?>
-
+		<?php get_template_part( 'content', 'page' ); ?>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
