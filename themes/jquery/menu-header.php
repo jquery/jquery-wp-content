bd103464b97afe503f025f5cc53bc6d4952223d5
Scott González <scott.gonzalez@gmail.com>
2012-12-24 15:29:54 -0500

    qunitjs.com: Re-order menu items.

--- themes/jquery/menu-header.php
+++ themes/jquery/menu-header.php
@@ -33,9 +33,9 @@ function menu_header_learn_jquery_com() {
 function menu_header_qunitjs_com() {
 	return array(
 		'http://qunitjs.com/' => 'Home',
-		'http://qunitjs.com/cookbook/' => 'Cookbook',
-		'http://api.qunitjs.com/' => 'API Documentation',
 		'http://qunitjs.com/intro/' => 'Intro to Unit Testing',
+		'http://api.qunitjs.com/' => 'API Documentation',
+		'http://qunitjs.com/cookbook/' => 'Cookbook',
 		'http://qunitjs.com/extensions/' => 'Extensions',
 	);
 }
