6d57b495e430fc31d3efda0208c4ad5d9702898f
Scott González <scott.gonzalez@gmail.com>
2012-12-24 16:02:58 -0500

    Added contentfull template.

---
+++ themes/jquery/page-contentfull.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Template Name: Content Full
+ */
+?>
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+<div class="content-full">
+	<div id="content">
+		<?php if ( !count( get_post_meta( $post->ID, "hide_title" ) ) ) : ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<hr>
+		<?php endif; ?>
+
+		<?php get_template_part( 'content', 'page' ); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
