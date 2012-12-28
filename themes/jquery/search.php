5c7baddc987cca1c490987a558d9cd47963720f4
Scott González <scott.gonzalez@gmail.com>
2012-12-28 12:49:02 -0500

    Clean up default search page. Fixes #138.

--- themes/jquery/search.php
+++ themes/jquery/search.php
@@ -2,46 +2,47 @@
 /**
  * The template for displaying Search Results pages.
  */
+?>
+<?php
+	global $sidebar;
+	get_header();
+?>
 
-get_header(); ?>
+<div class="content-right">
+	<div id="content">
+	<?php if ( have_posts() ) : ?>
 
-		<section id="body" class="clearfix">
-			<div class="inner" role="main">
+		<header class="page-header">
+			<h1 class="page-title"><?php
+				printf( __( 'Search Results for: %s', 'twentyeleven' ), '<span>' . get_search_query() . '</span>' );
+			?></h1>
+			<hr>
+		</header>
 
-			<?php if ( have_posts() ) : ?>
+		<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content' );
+			endwhile;
+		?>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyeleven' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header>
+		<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+	<?php else : ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+		<article id="post-0" class="post no-results not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+			</header>
 
-					<?php
-						get_template_part( 'content' );
-					?>
+			<div class="entry-content">
+				<p><?php _e( 'Apologies, but nothing matched your search criteria.', 'twentyeleven' ); ?></p>
+			</div>
+		</article>
 
-				<?php endwhile; ?>
+	<?php endif; ?>
+	</div>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria.', 'twentyeleven' ); ?></p>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- .inner -->
-		</section><!-- #body -->
+	<?php if ( $sidebar ) : get_sidebar(); endif; ?>
+</div>
 
 <?php get_footer(); ?>
