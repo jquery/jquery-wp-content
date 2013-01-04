7f7febff71de6af657e0ea4023bf7d753d1ef997
adam j. sontag <ajpiano@ajpiano.com>
2013-01-04 03:18:43 -0500

    learn.jquery.com: Use new css framework conventions for default page template for all articles

--- themes/learn.jquery.com/page.php
+++ themes/learn.jquery.com/page.php
@@ -1,23 +1,19 @@
 <?php get_header(); ?>
 
-  <!-- body -->
-  <?php global $sidebar; ?>
-  <div id="body" class="clearfix <?php echo $sidebar; ?>">
-    
-    <!-- inner -->
-    <div class="inner">
-				<div class="beta-ribbon-wrapper"><div class="beta-ribbon"><a href="/about/#beta">Beta</a></div></div>
-				<?php the_post(); ?>
+<?php global $sidebar; ?>
+<div class="content-right">
+	<div id="content">
+		<div class="beta-ribbon-wrapper"><div class="beta-ribbon"><a href="/about/#beta">Beta</a></div></div>
+		<?php the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-    </div>
-    <!-- /inner -->
-    
-    <?php if($sidebar): ?>
-    	<?php get_sidebar(); ?>
-    <?php endif; ?>
-    
-  </div>
-  <!-- /body -->
+		<?php get_template_part( 'content', 'page' ); ?>
+	</div>
+
+	<?php if($sidebar): ?>
+		<?php get_sidebar(); ?>
+	<?php endif; ?>
+
+</div>
+<!-- /body -->
 
 <?php get_footer(); ?>
