95ce8c88d2a73f470fd110dc8ba45d023d402195
Mike Pennisi <mike@mikepennisi.com>
2013-01-07 12:59:10 -0500

    Update jQuery to latest (1.8.3)
    This change is not necessary to fix any bugs, but it is intended to
    prevent confusion, particularly for users of `api.jquery.com` and
    `learn.jquery.com`. These sites describe the latest version of jQuery,
    so the globally-available jQuery should likewise be kept up-to-date.
    This will prevent any confusion that might otherwise arise when users
    experiment with jQuery from their browser's console.

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -36,8 +36,8 @@ $sidebar = "sidebar-left";
 <script src="<?php echo get_template_directory_uri(); ?>/js/selectivizr.js"></script>
 <![endif]-->
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.7.2.min.js"%3E%3C/script%3E'))</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.8.3.min.js"%3E%3C/script%3E'))</script>
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
