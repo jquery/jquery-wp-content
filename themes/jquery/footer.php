a575f9583a71acf13b1af90fd0fe109347cb8d00
Scott González <scott.gonzalez@gmail.com>
2013-01-07 14:28:36 -0500

    Footers: Comment out links to learn site until it launches.

--- themes/jquery/footer.php
+++ themes/jquery/footer.php
@@ -74,7 +74,7 @@
 
 		<div id="legal">
 			<ul class="footer-site-links">
-				<li><a class="icon-pencil" href="http://learn.jquery.com/">Learning Center</a></li>
+				<!-- <li><a class="icon-pencil" href="http://learn.jquery.com/">Learning Center</a></li> -->
 				<li><a class="icon-group" href="http://forum.jquery.com/">Forum</a></li>
 				<li><a class="icon-wrench" href="http://api.jquery.com/">API</a></li>
 				<li><a class="icon-twitter" href="http://twitter.com/jquery">Twitter</a></li>
