69a0e38828e3d831aa4ecba29e88c269aedaf9b5
adam j. sontag <ajpiano@ajpiano.com>
2013-01-08 10:22:17 -0500

    brand.jquery.org: Add trailing slashes to sidebar links before anchors

--- themes/brand.jquery.org/sidebar.php
+++ themes/brand.jquery.org/sidebar.php
@@ -7,56 +7,56 @@
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/logos">Logos</a></h3>
 		<ul>
-			<li><a href="/logos#the-mark">The mark</a></li>
-			<li><a href="/logos#spacing-guidelines">Spacing guidelines</a></li>
-			<li><a href="/logos#incorrect-logo-usage">Incorrect usage</a></li>
-			<li><a href="/logos#proper-usage">Proper usage</a></li>
+			<li><a href="/logos/#the-mark">The mark</a></li>
+			<li><a href="/logos/#spacing-guidelines">Spacing guidelines</a></li>
+			<li><a href="/logos/#incorrect-logo-usage">Incorrect usage</a></li>
+			<li><a href="/logos/#proper-usage">Proper usage</a></li>
 		</ul>
 	</aside>
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/colors">Colors</a></h3>
 		<ul>
-			<li><a href="/colors#jquery-black">jQuery black</a></li>
-			<li><a href="/colors#jquery-blue">jQuery blue</a></li>
-			<li><a href="/colors#jquery-ui-orange">jQuery UI orange</a></li>
-			<li><a href="/colors#jquery-mobile-green">jQuery Mobile green</a></li>
-			<li><a href="/colors#sizzle-orange-red">Sizzle orange/red</a></li>
-			<li><a href="/colors#qunit-purple">QUnit purple</a></li>
+			<li><a href="/colors/#jquery-black">jQuery black</a></li>
+			<li><a href="/colors/#jquery-blue">jQuery blue</a></li>
+			<li><a href="/colors/#jquery-ui-orange">jQuery UI orange</a></li>
+			<li><a href="/colors/#jquery-mobile-green">jQuery Mobile green</a></li>
+			<li><a href="/colors/#sizzle-orange-red">Sizzle orange/red</a></li>
+			<li><a href="/colors/#qunit-purple">QUnit purple</a></li>
 		</ul>
 	</aside>
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/typography">Typography</a></h3>
 		<ul>
-			<li><a href="/typography#klavika">Klavika</a></li>
-			<li><a href="/typography#helvetica">Helvetica</a></li>
-			<li><a href="/typography#quagmire">Quagmire</a></li>
+			<li><a href="/typography/#klavika">Klavika</a></li>
+			<li><a href="/typography/#helvetica">Helvetica</a></li>
+			<li><a href="/typography/#quagmire">Quagmire</a></li>
 		</ul>
 	</aside>
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/naming-conventions">Naming Conventions</a></h3>
 		<ul>
-			<li><a href="/naming-conventions#branding-your-tagline">Branding your tagline</a></li>
-			<li><a href="/naming-conventions#using-jquery-in-your-name">Using jQuery in your name</a></li>
-			<li><a href="/naming-conventions#what-do-avoid">What to avoid</a></li>
+			<li><a href="/naming-conventions/#branding-your-tagline">Branding your tagline</a></li>
+			<li><a href="/naming-conventions/#using-jquery-in-your-name">Using jQuery in your name</a></li>
+			<li><a href="/naming-conventions/#what-do-avoid">What to avoid</a></li>
 		</ul>
 	</aside>
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/events-conferences">Events &amp; Conferences</a></h3>
 		<ul>
-			<li><a href="/events-conferences#naming-your-jquery-event">Naming</a></li>
-			<li><a href="/events-conferences#promoting-your-jquery-event">Promoting</a></li>
-			<li><a href="/events-conferences#event-rules">Rules</a></li>
-			<li><a href="/events-conferences#jquery-developer-summit">jQuery Developer Summit</a></li>
+			<li><a href="/events-conferences/#naming-your-jquery-event">Naming</a></li>
+			<li><a href="/events-conferences/#promoting-your-jquery-event">Promoting</a></li>
+			<li><a href="/events-conferences/#event-rules">Rules</a></li>
+			<li><a href="/events-conferences/#jquery-developer-summit">jQuery Developer Summit</a></li>
 		</ul>
 	</aside>
 	<aside class="widget">
 		<h3 class="widget-title"><a href="/press-contact">Press &amp; Contact</a></h3>
 		<ul>
-			<li><a href="/press-contact#visit-jquery-online">Online</a></li>
-			<li><a href="/press-contact#jquery-social-networks">Facebook &amp; Twitter</a></li>
-			<li><a href="/press-contact#journalists">Journalists</a></li>
-			<li><a href="/press-contact#questions">Questions</a></li>
-			<li><a href="/press-contact#bugs">Bugs</a></li>
+			<li><a href="/press-contact/#visit-jquery-online">Online</a></li>
+			<li><a href="/press-contact/#jquery-social-networks">Facebook &amp; Twitter</a></li>
+			<li><a href="/press-contact/#journalists">Journalists</a></li>
+			<li><a href="/press-contact/#questions">Questions</a></li>
+			<li><a href="/press-contact/#bugs">Bugs</a></li>
 		</ul>
 	</aside>
 </div>
