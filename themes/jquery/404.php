840000bd1291e1e7b1c06ee3ee0ce9f5233f2b49
Scott González <scott.gonzalez@gmail.com>
2013-01-11 17:36:51 -0500

    Fix 404 markup.

--- themes/jquery/404.php
+++ themes/jquery/404.php
@@ -4,22 +4,18 @@
  */
 
 get_header(); ?>
+<div class="content-full full-width twelve columns">
+	<div id="content">
+		<article id="post-0" class="post error404 not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+				<hr>
+			</header>
 
-	<div id="body">
-		<div class="inner" role="main">
-
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
-				</header>
-
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for.', 'twentyeleven' ); ?></p>
-
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
-
-		</div><!-- .inner -->
-	</div><!-- #body -->
-
+			<div class="entry-content">
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for.', 'twentyeleven' ); ?></p>
+			</div>
+		</article>
+	</div>
+</div>
 <?php get_footer(); ?>
