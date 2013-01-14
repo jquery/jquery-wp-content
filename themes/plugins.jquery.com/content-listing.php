e0c6553a87abea75527886f533626e458f974614
Scott González <scott.gonzalez@gmail.com>
2013-01-13 22:56:44 -0500

    plugins.jquery.com: Use base tag template; clean up content listing template.

--- themes/plugins.jquery.com/content-listing.php
+++ themes/plugins.jquery.com/content-listing.php
@@ -1,13 +1,19 @@
-<article class="clearfix">
-	<h2 class="plugin-title">
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	</h2>
-	<p class="info">
-		Version <?php $latest = get_post_meta( $post->ID, 'latest' ); echo $latest[0]; ?> -
-		Released <?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago.'; ?>
-	</p>
-	<?php the_excerpt(); ?>
-	<div class="action">
-		<a class="button" href="<?php the_permalink(); ?>">View Plugin</a>
+<article class="hentry clearfix">
+	<header class="entry-header">
+		<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</h2>
+	</header>
+	<div class="entry-summary row collapse">
+		<div class="eight columns">
+			<p class="info">
+				Version <?php $latest = get_post_meta( $post->ID, 'latest' ); echo $latest[0]; ?> -
+				Released <?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago.'; ?>
+			</p>
+			<?php the_excerpt(); ?>
+		</div>
+		<div class="four columns">
+			<a class="button" href="<?php the_permalink(); ?>">View Plugin</a>
+		</div>
 	</div>
 </article>
