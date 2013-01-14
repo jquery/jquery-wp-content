6da1e558b7a09a9352000e25b18e08588cea442f
Scott González <scott.gonzalez@gmail.com>
2013-01-13 23:39:01 -0500

    plugins.jquery.com: Clean up plugin pages.

--- themes/plugins.jquery.com/single-jquery_plugin.php
+++ themes/plugins.jquery.com/single-jquery_plugin.php
@@ -1,37 +1,23 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
+<?php get_header(); ?>
+<?php the_post(); ?>
 
-get_header(); ?>
+<div class="content-left twelve columns">
+	<div id="content" class="jquery-plugin">
+		<?php if ( $post->post_parent && (jq_release_version() !== jq_release_version( $post->post_parent )) ) : ?>
+			<div class="notify">
+				<p class="icon-warning-sign">
+				<?php if ( jq_release_is_stable() ) : ?>
+					This version is old school, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest version</a>.
+				<?php else : ?>
+					This is a pre-release version, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest stable version</a>.
+				<?php endif; ?>
+				</p>
+			</div>
+		<?php endif; ?>
 
-<div id="content" class="jquery-plugin">
-	<?php the_post(); ?>
-	<?php if ( $post->post_parent && (jq_release_version() !== jq_release_version( $post->post_parent )) ) : ?>
-		<div class="notify">
-			<p><i class="icon-warning-sign"></i>
-			<?php if ( jq_release_is_stable() ) : ?>
-				<span>This version is old school, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest version</a>.</span>
-			<?php else : ?>
-				<span>This is a pre-release version, check out the <a href="<?php echo get_permalink( $post->post_parent ); ?>">latest stable version</a>.</span>
-			<?php endif; ?>
-			</p>
-		</div>
-	<?php endif; ?>
-
-	<?php get_template_part( 'content', 'jquery_plugin' ); ?>
-
-</div><!-- #content -->
-
-<?php get_template_part( 'sidebar', 'jquery_plugin' ); ?>
+		<?php get_template_part( 'content', 'jquery_plugin' ); ?>
+	</div>
+	<?php get_sidebar( 'jquery_plugin' ); ?>
+</div>
 
 <?php get_footer(); ?>
