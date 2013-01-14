ddcd270f7d35c18d595bb90baf1b528e4fdc0532
Scott González <scott.gonzalez@gmail.com>
2013-01-13 22:56:02 -0500

    Implemented base tag template.

--- themes/jquery/tag.php
+++ themes/jquery/tag.php
@@ -5,53 +5,49 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+<div class="content-right twelve columns">
+	<div id="content">
+	<?php if ( have_posts() ) : ?>
 
-			<?php if ( have_posts() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title"><?php
+				printf( __( 'Tagged: %s', 'twentyeleven' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+			?></h1>
+			<hr>
+			<?php
+				$tag_description = tag_description();
+				if ( ! empty( $tag_description ) ) {
+					echo apply_filters( 'tag_archive_meta',
+						'<div class="tag-archive-meta">' . $tag_description . '</div>' );
+				}
+			?>
+		</header>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Tag Archives: %s', 'twentyeleven' ), '<span>' . single_tag_title( '', false ) . '</span>' );
-					?></h1>
+		<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content', 'listing' );
+			endwhile;
+		?>
 
-					<?php
-						$tag_description = tag_description();
-						if ( ! empty( $tag_description ) )
-							echo apply_filters( 'tag_archive_meta', '<div class="tag-archive-meta">' . $tag_description . '</div>' );
-					?>
-				</header>
+		<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+	<?php else : ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+		<article id="post-0" class="post no-results not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+			</header>
 
-					<?php
-						get_template_part( 'content' );
-					?>
+			<div class="entry-content">
+				<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+				<?php get_search_form(); ?>
+			</div>
+		</article>
 
-				<?php endwhile; ?>
+	<?php endif; ?>
+	</div>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+	<?php get_sidebar(); ?>
+</div>
 
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
