c3ebe8d8ba2141360f3e1e0f03680df54fe38d1e
Scott González <scott.gonzalez@gmail.com>
2013-01-14 08:28:57 -0500

    Change default search page to use content-listing template; Remove API search templates.

--- themes/api.jquerymobile.com/search.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-get_header();
-get_template_part( 'search', 'api' );
-get_footer();
--- themes/jquery/search.php
+++ themes/jquery/search.php
@@ -18,7 +18,7 @@
 
 		<?php
 			while ( have_posts() ) : the_post();
-				get_template_part( 'content' );
+				get_template_part( 'content', 'listing' );
 			endwhile;
 		?>
 
