342f518f5c201975b86a8c329d6839cbd612b70b
Scott González <scott.gonzalez@gmail.com>
2013-01-14 20:58:07 -0500

    plugins.jquery.com: Only count tags on the latest version of each plugin. Thanks @nacin.

--- mu-plugins/plugins.jquery.com/posts.php
+++ mu-plugins/plugins.jquery.com/posts.php
@@ -50,3 +50,33 @@ add_action( 'pre_get_posts', 'jquery_plugin_posts_only_for_searches' );
 add_filter( 'pre_option_permalink_structure', function() {
 	return '/%postname%';
 } );
+
+/*
+ * Ensure that the post_tag taxonomy uses our own special counting logic.
+ *
+ * Normally, this would simply be specified by register_taxonomy() with
+ * the update_count_callback flag. Since we use the core tag taxonomy,
+ * let's just continue to hijack it.
+ */
+add_action( 'init', function() {
+	$GLOBALS['wp_taxonomies']['post_tag']->update_count_callback = 'jquery_update_plugin_tag_count';
+} );
+
+/**
+ * Mostly just _update_post_term_count(), tweaked for
+ * post_type = 'jquery_plugin' and post_parent = 0.
+ */
+function jquery_update_plugin_tag_count( $terms, $taxonomy ) {
+	global $wpdb;
+	foreach ( (array) $terms as $term ) {
+		$count = (int) $wpdb->get_var( $wpdb->prepare(
+			"SELECT COUNT(*) FROM $wpdb->term_relationships, $wpdb->posts " .
+			"WHERE $wpdb->posts.ID = $wpdb->term_relationships.object_id " .
+				"AND post_status = 'publish' AND post_type = 'jquery_plugin' " .
+				"AND post_parent = 0 AND term_taxonomy_id = %d",
+			$term ) );
+		do_action( 'edit_term_taxonomy', $term, $taxonomy );
+		$wpdb->update( $wpdb->term_taxonomy, compact( 'count' ), array( 'term_taxonomy_id' => $term ) );
+		do_action( 'edited_term_taxonomy', $term, $taxonomy );
+	}
+}
