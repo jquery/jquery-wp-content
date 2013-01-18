03325a583466849241261f6dbe142f0e4669860c
Scott González <scott.gonzalez@gmail.com>
2013-01-18 11:25:31 -0500

    Updated archive template.

--- themes/jquery/archive.php
+++ themes/jquery/archive.php
@@ -1,65 +1,44 @@
-<?php
-/**
- * The template for displaying Archive pages.
- *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="content-right twelve columns">
+	<div id="content">
 
-		<section id="body" class="clearfix content-right">
-			<div class="inner" role="main">
+	<?php if ( have_posts() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title">
+				<?php if ( is_day() ) : ?>
+					<?php printf( __( 'Daily Archives: %s', 'twentyeleven' ), '<span>' . get_the_date() . '</span>' ); ?>
+				<?php elseif ( is_month() ) : ?>
+					<?php printf( __( 'Monthly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( 'F Y' ) . '</span>' ); ?>
+				<?php elseif ( is_year() ) : ?>
+					<?php printf( __( 'Yearly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( 'Y' ) . '</span>' ); ?>
+				<?php else : ?>
+					<?php _e( 'Blog Archives', 'twentyeleven' ); ?>
+				<?php endif; ?>
+			</h1>
+			<hr class="dots">
+		</header>
 
-			<?php if ( have_posts() ) : ?>
+		<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content' );
+			endwhile;
+		?>
 
-				<header class="page-header">
-					<h1 class="page-title">
-						<?php if ( is_day() ) : ?>
-							<?php printf( __( 'Daily Archives: %s', 'twentyeleven' ), '<span>' . get_the_date() . '</span>' ); ?>
-						<?php elseif ( is_month() ) : ?>
-							<?php printf( __( 'Monthly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( 'F Y' ) . '</span>' ); ?>
-						<?php elseif ( is_year() ) : ?>
-							<?php printf( __( 'Yearly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( 'Y' ) . '</span>' ); ?>
-						<?php else : ?>
-							<?php _e( 'Blog Archives', 'twentyeleven' ); ?>
-						<?php endif; ?>
-					</h1>
-				</header>
+		<?php twentyeleven_content_nav( 'nav-below' ); ?>
+	<?php else : ?>
+		<article id="post-0" class="post no-results not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+			</header>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						get_template_part( 'content' );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive.', 'twentyeleven' ); ?></p>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- .inner -->
-
-			<?php get_sidebar(); ?>
-
-		</section><!-- #body -->
+			<div class="entry-content">
+				<p><?php _e( 'Apologies, but no results were found for the requested archive.', 'twentyeleven' ); ?></p>
+			</div>
+		</article>
+	<?php endif; ?>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
