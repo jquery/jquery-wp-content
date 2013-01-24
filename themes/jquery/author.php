0c10a6bfd4bf5346ef4b743eae4ef5966b41d3c0
Scott González <scott.gonzalez@gmail.com>
2013-01-24 09:16:12 -0500

    Implemented author template.

--- themes/jquery/author.php
+++ themes/jquery/author.php
@@ -5,77 +5,72 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+<div class="content-right twelve columns">
+	<div id="content">
+	<?php if ( have_posts() ) : ?>
 
-			<?php if ( have_posts() ) : ?>
+		<?php
+			/* Queue the first post, that way we know
+			 * what author we're dealing with (if that is the case).
+			 *
+			 * We reset this later so we can run the loop
+			 * properly with a call to rewind_posts().
+			 */
+			the_post();
+		?>
 
-				<?php
-					/* Queue the first post, that way we know
-					 * what author we're dealing with (if that is the case).
-					 *
-					 * We reset this later so we can run the loop
-					 * properly with a call to rewind_posts().
-					 */
-					the_post();
-				?>
+		<header class="page-header">
+			<h1 class="page-title author"><?php
+				printf( __( 'Author Archives: %s', 'twentyeleven' ), get_the_author() );
+			?></h1>
+		</header>
 
-				<header class="page-header">
-					<h1 class="page-title author"><?php printf( __( 'Author Archives: %s', 'twentyeleven' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' ); ?></h1>
-				</header>
+		<?php
+			/* Since we called the_post() above, we need to
+			 * rewind the loop back to the beginning that way
+			 * we can run the loop properly, in full.
+			 */
+			rewind_posts();
+		?>
 
-				<?php
-					/* Since we called the_post() above, we need to
-					 * rewind the loop back to the beginning that way
-					 * we can run the loop properly, in full.
-					 */
-					rewind_posts();
-				?>
+		<?php
+		// If a user has filled out their description, show a bio on their entries.
+		if ( get_the_author_meta( 'description' ) ) : ?>
+		<div id="author-info">
+			<div id="author-avatar">
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 60 ) ); ?>
+			</div>
+			<div id="author-description">
+				<h2><?php printf( __( 'About %s', 'twentyeleven' ), get_the_author() ); ?></h2>
+				<?php the_author_meta( 'description' ); ?>
+			</div>
+		</div>
+		<?php endif; ?>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+		<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content', 'listing' );
+			endwhile;
+		?>
 
-				<?php
-				// If a user has filled out their description, show a bio on their entries.
-				if ( get_the_author_meta( 'description' ) ) : ?>
-				<div id="author-info">
-					<div id="author-avatar">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 60 ) ); ?>
-					</div><!-- #author-avatar -->
-					<div id="author-description">
-						<h2><?php printf( __( 'About %s', 'twentyeleven' ), get_the_author() ); ?></h2>
-						<?php the_author_meta( 'description' ); ?>
-					</div><!-- #author-description	-->
-				</div><!-- #entry-author-info -->
-				<?php endif; ?>
+		<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+	<?php else : ?>
 
-					<?php
-						get_template_part( 'content' );
-					?>
+		<article id="post-0" class="post no-results not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+			</header>
 
-				<?php endwhile; ?>
+			<div class="entry-content">
+				<p><?php _e( 'Apologies, but no results were found for the requested archive.', 'twentyeleven' ); ?></p>
+			</div>
+		</article>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+	<?php endif; ?>
+	</div>
 
-			<?php else : ?>
+	<?php get_sidebar(); ?>
+</div>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
