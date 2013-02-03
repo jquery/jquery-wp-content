8be92248da2ca9517851244f2076252ada339ca3
Scott González <scott.gonzalez@gmail.com>
2013-02-02 19:09:45 -0500

    Changed the label for the search form submit button from 'go' to 'search'.

--- themes/jquery/searchform.php
+++ themes/jquery/searchform.php
@@ -4,7 +4,7 @@
  */
 ?>
 <form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<button type="submit" class="icon-search"><span class="visuallyhidden">go</span></button>
+	<button type="submit" class="icon-search"><span class="visuallyhidden">search</span></button>
 	<label>
 		<span class="visuallyhidden">Search <?php bloginfo( 'name' ); ?></span>
 		<input type="text" name="s" value="<?php echo get_search_query(); ?>"
