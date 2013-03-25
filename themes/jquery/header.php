e23fe7625012b95c6e2e3c72751759ff88b1230d
Scott González <scott.gonzalez@gmail.com>
2013-03-25 12:30:23 -0400

    Upgrade to jquery-migrate 1.1.1 and load locally. Fixes jquery/jquery.org#41.

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -32,7 +32,7 @@
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))</script>
-	<script src="http://code.jquery.com/jquery-migrate-1.0.0.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-migrate-1.1.1.min.js"></script>
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
