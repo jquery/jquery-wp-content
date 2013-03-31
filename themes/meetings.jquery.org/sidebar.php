60955c340e1271bb666c26a9ef8b6e4caa7da50a
Richard D. Worth <rdworth@gmail.com>
2013-03-30 22:40:50 -0500

    minutes. theme: Added 'Minutes' heading to categories list in sidebar

---
+++ themes/meetings.jquery.org/sidebar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The default sidebar lists categories, up to 2 levels deep.
+ */
+?>
+<div id="sidebar" class="widget-area" role="complementary">
+	<aside id="categories" class="widget">
+		<h2>Minutes</h2>
+		<ul>
+			<?php wp_list_categories( array(
+				'depth' => 2,
+				'title_li' => '',
+				'current_category' => jq_post_category(),
+				'use_desc_for_title' => false
+			) ); ?>
+		</ul>
+	</aside>
+</div>
