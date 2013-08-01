42aee35463d0f1c95eb1e61c1b5ee9443c4b7a91
jdorfman <justindorfman@gmail.com>
2013-08-01 00:40:25 -0700

    Powered by MaxCDN via Scott

--- themes/codeorigin.jquery.com/page.php
+++ themes/codeorigin.jquery.com/page.php
@@ -1,1 +1,17 @@
-<?php get_template_part( 'page', 'contentfull' ); ?>
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+<div class="content-full twelve columns">
+	<div id="content">
+		<?php if ( !count( get_post_meta( $post->ID, "hide_title" ) ) ) : ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h3>Powered by <a href="http://www.maxcdn.com" title="CDN" target="_blank">MaxCDN</a></h3>
+		<hr>
+		<?php endif; ?>
+
+		<?php get_template_part( 'content', 'page' ); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
