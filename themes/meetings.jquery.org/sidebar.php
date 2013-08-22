cd252cbeb686cc59ae2628ff901661382bb97b80
Scott González <scott.gonzalez@gmail.com>
2013-08-22 08:39:06 -0400

    meetings.jquery.org: Added link to TC39 Meeting Notes. Fixes jquery/meetings.jquery.org#8.

--- themes/meetings.jquery.org/sidebar.php
+++ themes/meetings.jquery.org/sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar" class="widget-area" role="complementary">
 	<aside id="categories" class="widget">
-		<h2>Minutes</h2>
+		<h2>Internal Minutes</h2>
 		<ul>
 			<?php wp_list_categories( array(
 				'depth' => 2,
@@ -8,6 +8,10 @@
 				'current_category' => jq_post_category(),
 				'use_desc_for_title' => false
 			) ); ?>
+		</ul>
+		<h2>External Minutes</h2>
+		<ul>
+			<li><a href="https://github.com/rwaldron/tc39-notes">TC39 (ECMAScript)</a></li>
 		</ul>
 	</aside>
 </div>
