63b12df447982699c5472fb59b941e951683b61a
Ghislain Seguin <ghislain@gmail.com>
2013-12-20 14:22:24 -0700

    Mobile Download Builder: Changed path to image zip

--- themes/jquerymobile.com/builder.js
+++ themes/jquerymobile.com/builder.js
@@ -167,7 +167,7 @@ $( function( $ ) {
 		},
 		refreshImageBundleLink = function() {
 			var branch = $( "#branch option:selected" ).val(),
-				url = "http://code.jquery.com/mobile/" + branch + "/jquery.mobile.images-" + branch + ".zip";
+				url = "../resources/download/jquery.mobile.images-" + branch + ".zip";
 
 			if ( branch === "master" ) {
 				url = "https://github.com/jquery/jquery-mobile/tree/master/css/themes/default/images";
