a91495a6be3a10f62520e7add7ef73a8d9607ac0
Andrew Nacin <andrewnacin@gmail.com>
2014-01-18 16:26:00 -0500

    Remove WordPress shortlink <head> and header.

--- mu-plugins/jquery-actions.php
+++ mu-plugins/jquery-actions.php
@@ -6,3 +6,8 @@ remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action( 'wp_head', 'rel_canonical' );
+
+// Remove shortlink <head> and header.
+remove_action( 'wp_head',             'wp_shortlink_wp_head', 10 );
+remove_action( 'template_redirect',   'wp_shortlink_header',  11 );
+
