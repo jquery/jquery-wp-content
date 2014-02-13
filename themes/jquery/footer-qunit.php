68fa4fd0a82a8d57bddb056ff7882cc5ebce12e9
Scott González <scott.gonzalez@gmail.com>
2014-02-13 14:28:17 -0800

    All Sites: Remove quick access from footer.
    Closes gh-212
    Closes gh-210
    Closes gh-294

--- themes/jquery/footer-qunit.php
+++ themes/jquery/footer-qunit.php
@@ -3,29 +3,6 @@
 
 <footer class="clearfix simple">
 	<div class="constrain">
-		<div class="row">
-			<div class="six columns centered">
-				<h3><span>Quick Access</span></h3>
-				<div class="cdn">
-					<strong>CDN <em>CSS</em></strong>
-					<input value="//code.jquery.com/qunit/qunit-1.14.0.css" readonly>
-				</div>
-				<div class="cdn">
-					<strong>CDN <em>JS</em></strong>
-					<input value="//code.jquery.com/qunit/qunit-1.14.0.js" readonly>
-				</div>
-				<div class="download">
-					<strong>Download QUnit 1.14.0:</strong>
-					<span><a href="http://code.jquery.com/qunit/qunit-1.14.0.js">qunit.js</a><a href="http://code.jquery.com/qunit/qunit-1.14.0.css">qunit.css</a></span>
-				</div>
-				<ul class="footer-icon-links">
-					<li><a class="icon-github" href="http://github.com/jquery/qunit">GitHub <small>QUnit <br>Source</small></a></li>
-					<li><a class="icon-group" href="http://forum.jquery.com/qunit-and-testing">Forum <small>Community <br>Support</small></a></li>
-					<li><a class="icon-warning-sign" href="http://github.com/jquery/qunit/issues">Bugs <small>Issue <br>Tracker</small></a></li>
-				</ul>
-			</div>
-		</div>
-
 		<div id="legal">
 			<ul class="footer-site-links">
 				<li><a class="icon-pencil" href="http://learn.jquery.com/">Learning Center</a></li>
