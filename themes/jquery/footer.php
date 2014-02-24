a6e973ab88bd35c3a27a199a9e3ef8a4a37de547
Scott González <scott.gonzalez@gmail.com>
2014-02-24 13:03:01 -0500

    All Sites: Consolidate footer links

--- themes/jquery/footer.php
+++ themes/jquery/footer.php
@@ -32,19 +32,7 @@
 			</div>
 		</div>
 
-		<div id="legal">
-			<ul class="footer-site-links">
-				<li><a class="icon-pencil" href="http://learn.jquery.com/">Learning Center</a></li>
-				<li><a class="icon-group" href="http://forum.jquery.com/">Forum</a></li>
-				<li><a class="icon-wrench" href="http://api.jquery.com/">API</a></li>
-				<li><a class="icon-twitter" href="http://twitter.com/jquery">Twitter</a></li>
-				<li><a class="icon-comments" href="http://irc.jquery.org/">IRC</a></li>
-			</ul>
-			<p class="copyright">
-				Copyright <?php echo date('Y'); ?> <a href="https://jquery.org/team/">The jQuery Foundation</a>.<br>
-				<span class="sponsor-line"><a href="http://mediatemple.net" rel="noindex,nofollow" class="mt-link">Web hosting by Media Temple</a> | <a href="http://www.maxcdn.com" rel="noindex,nofollow" class="mc-link">CDN by MaxCDN</a> | <a href="http://wordpress.org/" class="wp-link">Powered by WordPress</a> | Thanks: <a href="https://jquery.org/members/">Members</a>, <a href="https://jquery.org/sponsors/">Sponsors</a></span>
-			</p>
-		</div>
+		<?php get_template_part( 'footer', 'bottom' ); ?>
 	</div>
 </footer>
 
