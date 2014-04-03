0ec865c6678be386403b2a70a30ac25bb1ce3e29
Scott González <scott.gonzalez@gmail.com>
2014-04-03 17:20:27 -0400

    learn.jquery.com: Cleanup

--- themes/learn.jquery.com/content-page.php
+++ themes/learn.jquery.com/content-page.php
@@ -1,15 +1,9 @@
-<?php
-/**
- * The template used for displaying page content in page.php
- */
-?>
-
 <?php $next_prev_arr = get_next_prev_post(); ?>
 <?php echo jq_post_heirarchy(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
+	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -19,7 +13,7 @@
 				<?php wp_list_pages("title_li=&sort_column=menu_order&child_of=" . $post->ID); ?>
 			</ul>
 		<?php }?>
-	</div><!-- .entry-content -->
+	</div>
 
 	<hr>
 
@@ -53,28 +47,28 @@
 		</aside>
 		<aside class="github-feedback six columns">
 			<h3>Suggestions, Problems, Feedback?</h3>
-			<a class="button dark" href="<?php echo jq_get_github_url(); ?>"><i class="icon-github"></i>  Open an Issue or Submit a Pull Request on GitHub</a>
+			<a class="button dark" href="<?php echo jq_get_github_url(); ?>"><i class="icon-github"></i> Open an Issue or Submit a Pull Request on GitHub</a>
 		</aside>
 
 		<?php if (isset($next_prev_arr['prev']) || isset($next_prev_arr['next']) ): ?>
 		<div class="bottom-links row">
-		    <?php if (isset($next_prev_arr['prev'])): ?>
-			   <div class="prev six columns">
-			       <a href="<?php echo $next_prev_arr['prev']->guid; ?>">
-				   <i class="icon-chevron-left"></i>
-				   <?php echo $next_prev_arr['prev']->post_title; ?>
-			       </a>
-			   </div>
-		    <?php endif; ?>
-		    <?php if (isset($next_prev_arr['next'])): ?>
-			   <div class="next six columns">
-			       <a href="<?php echo $next_prev_arr['next']->guid; ?>">
-				   <?php echo $next_prev_arr['next']->post_title; ?>
-				   <i class="icon-chevron-right"></i>
-			       </a>
-			   </div>
-		    <?php endif; ?>
+			<?php if (isset($next_prev_arr['prev'])): ?>
+				<div class="prev six columns">
+					<a href="<?php echo $next_prev_arr['prev']->guid; ?>">
+					<i class="icon-chevron-left"></i>
+					<?php echo $next_prev_arr['prev']->post_title; ?>
+					</a>
+				</div>
+			<?php endif; ?>
+			<?php if (isset($next_prev_arr['next'])): ?>
+				<div class="next six columns">
+					<a href="<?php echo $next_prev_arr['next']->guid; ?>">
+					<?php echo $next_prev_arr['next']->post_title; ?>
+					<i class="icon-chevron-right"></i>
+					</a>
+				</div>
+			<?php endif; ?>
 		</div>
 		<?php endif; ?>
-	</div><!-- .entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	</div>
+</article>
