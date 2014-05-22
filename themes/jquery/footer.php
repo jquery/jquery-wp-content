a4a0f274f63e200b5c3cb765adcba3066f8aa04e
Dan <danwellman@hotmail.com>
2014-05-22 18:17:24 +0100

    jquery.com/jqueryui.com: Random book selector
    Minified widget source added to plugins.js (unminified version committed
    to js dir for reference). Updated jquery and jquery ui footers to init
    the widget.Removed old unused book images and added images for new
    books. Added JSON files for listed books. Proposed solution to
    https://github.com/jquery/jquery-wp-content/issues/290

--- themes/jquery/footer.php
+++ themes/jquery/footer.php
@@ -4,38 +4,19 @@
 <footer class="clearfix simple">
 	<div class="constrain">
 		<div class="row">
-			<div class="six columns offset-by-three">
-				<h3><span>Books</span></h3>
-				<ul class="books">
-					<li>
-						<a href="http://www.packtpub.com/learning-jquery-with-simple-javascript-techniques-fourth-edition/book">
-							<img src="<?php echo get_template_directory_uri(); ?>/content/books/learning-jquery-4th-ed.jpg" alt="Learning jQuery 4th Edition by Karl Swedberg and Jonathan Chaffer" width="92" height="114">
-							Learning jQuery Fourth Edition
-							<cite>Karl Swedberg and Jonathan Chaffer</cite>
-						</a>
-					</li>
-					<li>
-						<a href="http://www.manning.com/derosa?a_aid=141d9491&a_bid=bdd5b7ad">
-							<img src="<?php echo get_template_directory_uri(); ?>/content/books/jquery-in-action.jpg" alt="jQuery in Action by Bear Bibeault, Yehuda Katz, and Aurelio De Rosa" width="92" height="114">
-							jQuery in Action
-							<cite>Bear Bibeault, Yehuda Katz, and Aurelio De Rosa</cite>
-						</a>
-					</li>
-					<li>
-						<a href="http://www.syncfusion.com/resources/techportal/ebooks/jquery?utm_medium=BizDev-jQuery.org0513">
-							<img src="<?php echo get_template_directory_uri(); ?>/content/books/jquery-succinctly.jpg" alt="jQuery Succinctly by Cody Lindley" width="92" height="114">
-							jQuery Succinctly
-							<cite>Cody Lindley</cite>
-						</a>
-					</li>
-				</ul>
-			</div>
+			<div id="books" class="six columns offset-by-three"></div>
 		</div>
-
 		<?php get_template_part( 'footer', 'bottom' ); ?>
 	</div>
 </footer>
-
+<script>
+	$(function() {
+		var urlPrefix = '<?php echo get_template_directory_uri(); ?>';
+		$.when($.getJSON(urlPrefix + '/content/books/jquery-book-data.json')).then(function (data) {
+			randomImageSelector.init(data.books, { urlPrefix: urlPrefix });
+		});
+	});
+</script>
 <?php wp_footer(); ?>
 
 </body>
