309c3bff1913b8cd22b4069e8efe1178400d9751
Scott González <scott.gonzalez@gmail.com>
2014-08-25 12:46:45 -0400

    events.jquery.org: Add page template for UK 2013

---
+++ themes/events.jquery.org/page-uk-2013.php
@@ -0,0 +1,338 @@
+<?php
+/*
+ * Template Name: jQuery UK 2013
+ */
+the_post();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jQuery UK Conference - Oxford 2013</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<link href="/resources/2013/uk/styles/site.css?v=3" rel="stylesheet" media="screen">
+
+	<!--[if lt IE 9]>
+		<script src="/resources/2013/uk/js/html5shiv.js"></script>
+	<![endif]-->
+
+	<!-- Adds js class when enabled -->
+	<script type="text/javascript">
+		document.getElementsByTagName('html')[0].setAttribute('class', 'js');
+	</script>
+
+	<!-- Analytics -->
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-26445173-2']);
+		_gaq.push(['_setDomainName', 'events.jquery.org']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+	<!-- Pretty fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<div id="wonderland" class="wonderland scene scene-location">
+	<div class="stage">
+		<div class="stage-inner">
+
+			<div class="cloud cloud-a"></div>
+			<div class="cloud cloud-b"></div>
+			<div class="cloud cloud-c"></div>
+
+			<div class="wave-left-right delayed">
+				<div class="wave-up-down delayed">
+					<div class="wave wave-back"></div>
+				</div>
+			</div>
+
+			<div class="layer layer-below">
+				<div class="group group-rabbit">
+					<div class="boating-rabbit"></div>
+				</div>
+
+				<div class="fishy-left-right">
+					<div class="fishy"></div>
+				</div>
+			</div>
+
+			<div class="layer layer-above">
+				<div class="birdy-left-right">
+					<div class="birdy-left-right faster">
+						<div class="birdy-up-down">
+							<div class="birdy-flip">
+								<div class="birdy"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="tree tree-left">
+				<div class="sign-scene sign-white-rabbit"><div></div></div>
+				<div class="sign-scene sign-tweedles"><div></div></div>
+				<div class="sign-scene sign-croquet"><div></div></div>
+				<div class="sign-scene sign-tea-party"><div></div></div>
+				<div class="sign-scene sign-jabberwocky"><div></div></div>
+
+				<div class="cheshire-cat cheshire-cat-a">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+				<div class="cheshire-cat cheshire-cat-b">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+
+				<div class="cheshire-cat cheshire-cat-c">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+			</div>
+			<div class="tree tree-right">
+				<div class="cheshire-cat cheshire-cat-a">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+				<div class="cheshire-cat cheshire-cat-b">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+
+				<div class="cheshire-cat cheshire-cat-c">
+					<div class="cheshire-cat-body"></div>
+					<div class="cheshire-cat-head"></div>
+					<div class="cheshire-cat-grin"></div>
+				</div>
+			</div>
+
+			<div class="layer layer-above">
+				<div class="jabberwocky">
+					<div class="jabberwocky-arm-back">
+						<div class="jabberwocky-claw-back"></div>
+					</div>
+					<div class="jabberwocky-wing-back"></div>
+					<div class="jabberwocky-wing-front"></div>
+					<div class="jabberwocky-body"></div>
+					<div class="jabberwocky-arm-front">
+						 <div class="jabberwocky-claw-front"></div>
+					</div>
+					<div class="jabberwocky-head">
+						<div class="jabberwocky-jaw"></div>
+						<div class="jabberwocky-flames"></div>
+						<div class="jabberwocky-skull"></div>
+						<div class="jabberwocky-teeth"></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="sign-jquery"></div>
+
+			<div class="layer layer-below">
+
+				<div class="group group-tweedles">
+					<div class="walrus"></div>
+					<div class="island island-tweedles"></div>
+					<div class="tweedle tweedledee"></div>
+					<div class="tweedle tweedledum"></div>
+				</div>
+
+				<div class="group group-croquet">
+					<div class="island island-croquet"></div>
+					<div class="dodo"></div>
+					<div class="hedgehog-hoop"></div>
+					<div class="hedgehog-scared"></div>
+					<div class="caddy-bag"></div>
+					<div class="flamingo"></div>
+				</div>
+
+				<div class="group group-tea-party">
+					<div class="island island-tea-party"></div>
+
+					<div class="mad-hatter-leg-back"></div>
+					<div class="mad-hatter"></div>
+					<div class="mad-hatter-leg-front"></div>
+
+					<div class="dormouse"></div>
+
+					<div class="march-hare"></div>
+					<div class="march-hare-leg-back"></div>
+					<div class="march-hare-leg-front"></div>
+				</div>
+			</div>
+
+			<div class="layer layer-above">
+					<div class="teapot teapot-a">
+						<div class="teapot-inner">
+							<div class="tea-filter"><div class="tea"></div></div>
+						</div>
+					</div>
+					<div class="teapot teapot-b">
+						<div class="teapot-inner">
+							<div class="tea-filter"><div class="tea"></div></div>
+						</div>
+					</div>
+					<div class="teapot teapot-c">
+						<div class="teapot-inner">
+							<div class="tea-filter"><div class="tea"></div></div>
+						</div>
+					</div>
+			</div>
+
+			<div class="wave-left-right">
+				<div class="wave-up-down">
+					<div class="wave wave-front"></div>
+				</div>
+			</div>
+
+			<div class="sign-oxford"></div>
+			<a class="sign-ticket" href="http://jquk2013.eventbrite.co.uk/"><span></span></a>
+		</div>
+	</div>
+</div>
+
+<nav>
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<ul class="nav pushstate">
+					<li <?php if ( $post->post_name === 'uk' ) echo 'class="active"'; ?>><a href="/2013/uk/">Home</a></li>
+					<li <?php if ( $post->post_name === 'schedule' ) echo 'class="active"'; ?>><a href="/2013/uk/schedule">Programme</a></li>
+					<li <?php if ( $post->post_name === 'workshops' ) echo 'class="active"'; ?>><a href="/2013/uk/workshops">Workshops</a></li>
+					<li <?php if ( $post->post_name === 'location' ) echo 'class="active"'; ?>><a href="/2013/uk/location">Location</a></li>
+					<li <?php if ( $post->post_name === 'extras' ) echo 'class="active"'; ?>><a href="/2013/uk/extras">Fringe</a></li>
+					<li class="hidden-desktop hidden-tablet buy-tickets"><a href="http://jquk2013.eventbrite.co.uk/">Buy tickets</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
+
+<div id="content">
+	<?php the_content(); ?>
+
+	<footer class="primary">
+		<div class="container">
+			<h2>Thanks to our wonderful sponsors</h2>
+			<div class="row sponsors">
+				<div class="span2">
+					<a href="http://www.blackberry.com"><img src="/resources/2013/uk/assets/img/sponsors/blackberry.png" alt="Blackberry" /></a>
+					<span>Spotlight</span>
+				</div>
+				<div class="span2">
+					<a href="http://kendoui.com"><img src="/resources/2013/uk/assets/img/sponsors/kendo.png" alt="Kendo UI" /></a>
+					<span>Key</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.webapplicationsuk.com/"><img src="/resources/2013/uk/assets/img/sponsors/web-apps-uk.png" alt="Web Applications UK" /></a>
+					<span>Key</span>
+				</div>
+				<div class="span2">
+					<a href="http://cloudant.com"><img src="/resources/2013/uk/assets/img/sponsors/cloudant.png" alt="Cloudant" /></a>
+					<span>Key</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.infragistics.com/products/jquery/"><img src="/resources/2013/uk/assets/img/sponsors/infragistics_ignite.png" alt="Infragistics" /></a>
+					<span>Key</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.booking.com/"><img src="/resources/2013/uk/assets/img/sponsors/bookingdotcom.png" alt="Booking.com" /></a>
+					<span>Key</span>
+				</div>
+			</div>
+			<div class="row sponsors">
+				<div class="span2">
+					<a href="http://www.guardian.co.uk"><img src="/resources/2013/uk/assets/img/sponsors/guardian.png" alt="Guardian" /></a>
+					<span>Supporter</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.footballradar.com/"><img src="/resources/2013/uk/assets/img/sponsors/football-radar.png" alt="Football Radar" /></a>
+					<span>Supporter</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.appendto.com"><img src="/resources/2013/uk/assets/img/sponsors/appendto.png" alt="AppendTo" /></a>
+					<span>Training Sponsor</span>
+				</div>
+				<div class="span2">
+					<a href="http://www.mailchimp.com"><img src="/resources/2013/uk/assets/img/sponsors/mailchimp.png" alt="AppendTo" /></a>
+					<span>Supporter</span>
+				</div>
+				<div class="span2">
+					<a href="http://html.adobe.com/edge/phonegap-build/?promoid=KDLTH"><img src="/resources/2013/uk/assets/img/sponsors/phonegap.png" alt="Phonegap" /></a>
+					<span>Supporter</span>
+				</div>
+				<div class="span2">
+					<img src="/resources/2013/uk/assets/img/sponsors/sponsor-spacer.jpg" alt="" />
+				</div>
+			</div>
+			<p>
+				We have a range of sponsorship opportunities available, which you can find out more about in our sponsor pack.
+				<br />
+				Please get in touch with John Wards, our Technical Director, by email (
+				<a href="mailto:john@whiteoctober.co.uk">john@whiteoctober.co.uk</a>
+				) or call him on
+				<a href="tel:+440207 976 4894">+44(0)207 976 4894</a>
+				, to discuss sponsoring the event.
+			</p>
+			<p class="digi-oxford">
+				<a href="http://digitaloxford.com/week"><img src="/resources/2013/uk/assets/img/digitaloxfordbadge.png" alt="Digital Oxford Week"></a>
+			</p>
+			<p>Proud to be part of <a href="http://digitaloxford.com/week">Digital Oxford Week</a></p>
+		</div>
+	</footer>
+	<footer class="secondary">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<ul class="unstyled">
+						<li>
+							<a href="http://twitter.com/jquk" class="twitter">Follow @jquk</a>
+						</li>
+						<li>
+							<a href="http://lanyrd.com/2013/jquery-uk/" class="lanyrd">Track us on Lanyrd</a>
+						</li>
+						<li>
+							<a href="mailto:john@whiteoctober.co.uk" class="email">Send us an email</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<footer class="tertiary">
+		<hr class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<a href="http://www.whiteoctober.co.uk"><img src="/resources/2013/uk/assets/img/wo-logo.png" alt="Web and Mobile App Designers - Oxford - London - UK" /></a>
+					<p>&copy; 2013 <a href="http://www.whiteoctober.co.uk">White October</a></p>
+					<p>
+						jQuery UK is organised by <a href="http://www.whiteoctober.co.uk">White October</a>, an Oxford-based digital agency
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+
+<script src="/resources/2013/uk/js/history.js"></script>
+<script src="/resources/2013/uk/js/app.min.js?v=3"></script>
+
+</body>
+</html>
