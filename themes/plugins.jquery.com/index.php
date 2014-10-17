efafc664b80822f22f5dd3f3413e72c885b98dda
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-17 17:29:47 +0200

    plugins.jquery.com: Cleanup sidebar, return popular tags in index
    Closes gh-320

--- themes/plugins.jquery.com/index.php
+++ themes/plugins.jquery.com/index.php
@@ -7,9 +7,17 @@
 	</div>
 
 	<div id="content">
-		<h2 class="center-txt">The jQuery Plugin Registry is in read-only mode.</h2>
-		<h2 class="center-txt">New plugin releases will not be processed.</h2>
-		<p class="center-txt">We recommend moving to npm, using "<a href="https://www.npmjs.org/browse/keyword/jquery-plugin">jquery-plugin</a>" as the keyword in your package.json.</p>
+		<div class="three columns">
+			<aside class="widget">
+				<h3><i class="icon-tags"></i>Popular Tags</h3>
+				<?php jq_popular_tags(); ?>
+			</aside>
+		</div>
+		<div class="nine columns">
+			<h2 class="center-txt">The jQuery Plugin Registry is in read-only mode.</h2>
+			<h2 class="center-txt">New plugin releases will not be processed.</h2>
+			<p class="center-txt">We recommend moving to npm, using "<a href="https://www.npmjs.org/browse/keyword/jquery-plugin">jquery-plugin</a>" as the keyword in your package.json.</p>
+		</div>
 	</div>
 </div>
 
--- themes/plugins.jquery.com/sidebar.php
+++ themes/plugins.jquery.com/sidebar.php
@@ -1,13 +1,5 @@
 <div id="sidebar" class="widget-area" role="complementary">
 	<aside class="widget">
-		<h3 class="widget-title"><i class="icon-star"></i>New Plugins</h3>
-		<?php jq_new_plugins(); ?>
-	</aside>
-	<aside class="widget">
-		<h3 clas="widget-title"><i class="icon-calendar"></i>Recent Updates</h3>
-		<?php jq_updated_plugins(); ?>
-	</aside>
-	<aside class="widget">
 		<h3 class="widget-title"><i class="icon-tags"></i>Popular Tags</h3>
 		<?php jq_popular_tags(); ?>
 	</aside>
