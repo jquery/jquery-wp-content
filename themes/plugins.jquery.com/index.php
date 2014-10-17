9034716a47b0a545907f71ac43cbf2591646a8ae
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-17 17:24:02 +0200

    Plugins: Recommend using npm with jquery-plugin keyword

--- themes/plugins.jquery.com/index.php
+++ themes/plugins.jquery.com/index.php
@@ -9,6 +9,7 @@
 	<div id="content">
 		<h2 class="center-txt">The jQuery Plugin Registry is in read-only mode.</h2>
 		<h2 class="center-txt">New plugin releases will not be processed.</h2>
+		<p class="center-txt">We recommend moving to npm, using "<a href="https://www.npmjs.org/browse/keyword/jquery-plugin">jquery-plugin</a>" as the keyword in your package.json.</p>
 	</div>
 </div>
 
