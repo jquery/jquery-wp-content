4ab6dda9837cbde93606efbea0137f279ada4360
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-17 17:58:48 +0200

    QUnit: Add relevant books in footer
    Fixes jquery/qunitjs.com#65
    Closes #321

--- themes/jquery/footer-qunit.php
+++ themes/jquery/footer-qunit.php
@@ -1,8 +1,36 @@
 	</div>
 </div>
-
 <footer class="clearfix simple">
 	<div class="constrain">
+		<div class="row">
+			<div class="six columns offset-by-three">
+				<h3><span>Books</span></h3>
+				<ul class="books">
+					<li>
+						<a href="https://www.packtpub.com/application-development/instant-testing-qunit-instant">
+							<img src="<?php echo get_template_directory_uri(); ?>/content/books/qunit-instant-testing.jpg" alt="Instant Testing with QUnit" width="91" height="114">
+							<span class="book-title">Instant Testing with QUnit</span>
+							<cite>Dmitry Sheiko</cite>
+						</a>
+					</li>
+					<li>
+						<a href="http://shop.oreilly.com/product/0636920024699.do">
+							<img src="<?php echo get_template_directory_uri(); ?>/content/books/qunit-testable-javascript.gif" alt="Testable JavaScript" width="91" height="114">
+							<span class="book-title">Testable JavaScript</span>
+							<cite>Mark Ethan Trostler</cite>
+						</a>
+					</li>
+					<li>
+						<a href="http://tddjs.com/">
+							<img src="<?php echo get_template_directory_uri(); ?>/content/books/qunit-tddjs.png" alt="Test-Driven JavaScript Development" width="91" height="114">
+							<span class="book-title">Test-Driven JavaScript Development</span>
+							<cite>Christian Johansen</cite>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
 		<?php get_template_part( 'footer', 'bottom' ); ?>
 	</div>
 </footer>
