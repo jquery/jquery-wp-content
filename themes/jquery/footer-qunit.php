4e1dad201f9bdbc5c31141409d3f93c943426e91
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-17 17:58:48 +0200

    QUnit: Add testing book in footer
    Fixes jquery/qunitjs.com#65

--- themes/jquery/footer-qunit.php
+++ themes/jquery/footer-qunit.php
@@ -1,8 +1,22 @@
 	</div>
 </div>
-
 <footer class="clearfix simple">
 	<div class="constrain">
+		<div class="row">
+			<div class="six columns offset-by-three">
+				<h3><span>Books</span></h3>
+				<ul class="books">
+					<li>
+						<a href="https://www.packtpub.com/application-development/instant-testing-qunit-instant">
+							<img src="<?php echo get_template_directory_uri(); ?>/content/books/qunit-instant-testing.jpg" alt="Instant Testing with QUnit" width="91" height="114">
+							<span class="book-title">Instant Testing with QUnit</span>
+							<cite>Dmitry Sheiko</cite>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
 		<?php get_template_part( 'footer', 'bottom' ); ?>
 	</div>
 </footer>
