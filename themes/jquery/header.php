6d7f7685fe230466b70bb14bd0fdb22faffb4d1e
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-01-01 13:56:09 +0100

    Base theme: update to latest Modernizr
    Closes gh-331

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -26,7 +26,7 @@
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/base.css?v=1">
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 
-	<script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.custom.2.6.2.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.custom.2.8.3.min.js"></script>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.9.1.min.js"><\/script>')</script>
