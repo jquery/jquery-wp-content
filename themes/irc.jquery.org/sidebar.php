d5a90acac7057fc71344372fe842bee5d11ab817
Ryan Neufeld <ryan.neufeld@cbsinteractive.com>
2015-01-13 12:35:28 -0800

    irc.jquery.org: Add new IRC log channels
    Add new IRC channels.
    Sort list alphabetically.
    
    fixes jquery/irc.jquery.org#15

--- themes/irc.jquery.org/sidebar.php
+++ themes/irc.jquery.org/sidebar.php
@@ -2,15 +2,18 @@
 	<aside class="widget">
 		<h3 class="widget-title">Logs by Channel</h3>
 		<ul>
+			<li><a href="/%23css-chassis/">#css-chassis</a></li>
+			<li><a href="/%23globalize/">#globalize</a></li>
 			<li><a href="/%23jquery/">#jquery</a></li>
-			<li><a href="/%23jquery-dev/">#jquery-dev</a></li>
-			<li><a href="/%23jqueryui-dev/">#jqueryui-dev</a></li>
-			<li><a href="/%23jquery-meeting/">#jquery-meeting</a></li>
-			<li><a href="/%23jquery-infrastructure/">#jquery-infrastructure</a></li>
 			<li><a href="/%23jquery-content/">#jquery-content</a></li>
+			<li><a href="/%23jquery-dev/">#jquery-dev</a></li>
+			<li><a href="/%23jquery-infrastructure/">#jquery-infrastructure</a></li>
+			<li><a href="/%23jquery-meeting/">#jquery-meeting</a></li>
 			<li><a href="/%23jquerymobile-dev/">#jquerymobile-dev</a>
 				(inactive; moved to #jqueryui-dev)
 			</li>
+			<li><a href="/%23jqueryui-dev/">#jqueryui-dev</a></li>
+			<li><a href="/%23pep/">#pep</a></li>
 		</ul>
 	</aside>
 </div>
