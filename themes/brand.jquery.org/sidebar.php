0b72f5d555f14f6d902ab0dff257be078e8ccf41
Acho Arnold <arnold@archlinux.info>
2015-04-19 23:32:52 +0100

    brand.jquery.org: fix unnecessary redirects
    Closes gh-360

--- themes/brand.jquery.org/sidebar.php
+++ themes/brand.jquery.org/sidebar.php
@@ -5,7 +5,7 @@
 ?>
 <div id="sidebar" class="widget-area" role="complementary">
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/logos">Logos</a></h3>
+		<h3 class="widget-title"><a href="/logos/">Logos</a></h3>
 		<ul>
 			<li><a href="/logos/#the-mark">The mark</a></li>
 			<li><a href="/logos/#spacing-guidelines">Spacing guidelines</a></li>
@@ -14,7 +14,7 @@
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/colors">Colors</a></h3>
+		<h3 class="widget-title"><a href="/colors/">Colors</a></h3>
 		<ul>
 			<li><a href="/colors/#jquery-black">jQuery black</a></li>
 			<li><a href="/colors/#jquery-blue">jQuery blue</a></li>
@@ -26,7 +26,7 @@
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/typography">Typography</a></h3>
+		<h3 class="widget-title"><a href="/typography/">Typography</a></h3>
 		<ul>
 			<li><a href="/typography/#klavika">Klavika</a></li>
 			<li><a href="/typography/#helvetica">Helvetica</a></li>
@@ -34,7 +34,7 @@
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/naming-conventions">Naming Conventions</a></h3>
+		<h3 class="widget-title"><a href="/naming-conventions/">Naming Conventions</a></h3>
 		<ul>
 			<li><a href="/naming-conventions/#branding-your-tagline">Branding your tagline</a></li>
 			<li><a href="/naming-conventions/#using-jquery-in-your-name">Using jQuery in your name</a></li>
@@ -42,7 +42,7 @@
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/events-conferences">Events &amp; Conferences</a></h3>
+		<h3 class="widget-title"><a href="/events-conferences/">Events &amp; Conferences</a></h3>
 		<ul>
 			<li><a href="/events-conferences/#naming-your-jquery-event">Naming</a></li>
 			<li><a href="/events-conferences/#promoting-your-jquery-event">Promoting</a></li>
@@ -50,7 +50,7 @@
 		</ul>
 	</aside>
 	<aside class="widget">
-		<h3 class="widget-title"><a href="/press-contact">Press &amp; Contact</a></h3>
+		<h3 class="widget-title"><a href="/press-contact/">Press &amp; Contact</a></h3>
 		<ul>
 			<li><a href="/press-contact/#visit-jquery-online">Online</a></li>
 			<li><a href="/press-contact/#jquery-social-networks">Facebook &amp; Twitter</a></li>
