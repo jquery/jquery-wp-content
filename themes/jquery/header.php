575a8cf15ef18ce71230c5b294cff1220fee86dc
Michał Gołębiowski <m.goleb@gmail.com>
2015-11-09 19:48:31 +0100

    jquery: Update jQuery from 1.11.2 to 1.11.3, switch to our CDN
    Closes #381

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -28,8 +28,7 @@
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.custom.2.8.3.min.js"></script>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
