0d5db46d495cf9e741050819ac9609b3ad6f4e7f
Josh Mervine <joshua@mervine.net>
2016-02-09 18:59:01 -0800

    wrapping modal js in an iife

--- themes/jquery/js/sri-modal.js
+++ themes/jquery/js/sri-modal.js
@@ -1,53 +1,55 @@
-// Store modal templates
-var modalTemplate = $( "#sri-modal-template" )[ 0 ].outerHTML,
-	clipboard;
+(function() {
+	// Store modal templates
+	var modalTemplate = $( "#sri-modal-template" )[ 0 ].outerHTML,
+		clipboard;
 
-// Show modal on click
-$( "body" ).on( "click", ".open-sri-modal", function( event ) {
-	if ( !event.ctrlKey && !event.metaKey ) {
-		$( replace( modalTemplate, {
-			link: document.location.origin + $( this ).attr( "href" ),
-			hash: $( this ).attr( "data-hash" )
-		} ) ).removeAttr( "id" ).appendTo( "body" ).dialog( {
-			modal: true,
-			resizable: false,
-			width: 830,
-			dialogClass: "sri-modal",
-			draggable: false,
-			close: function() {
-				$( this ).remove();
-			}
-		} );
+	// Show modal on click
+	$( "body" ).on( "click", ".open-sri-modal", function( event ) {
+		if ( !event.ctrlKey && !event.metaKey ) {
+			$( replace( modalTemplate, {
+				link: document.location.origin + $( this ).attr( "href" ),
+				hash: $( this ).attr( "data-hash" )
+			} ) ).removeAttr( "id" ).appendTo( "body" ).dialog( {
+				modal: true,
+				resizable: false,
+				width: 830,
+				dialogClass: "sri-modal",
+				draggable: false,
+				close: function() {
+					$( this ).remove();
+				}
+			} );
 
-		return false;
-	}
-} );
-
-// Helper functions
-function replace ( string, values ) {
-	$.each( values, function( key, value ) {
-		string = string.replace( new RegExp( "\\{\\{" + key + "}}", "g" ), value );
+			return false;
+		}
 	} );
 
-	return string;
-}
-
-clipboard = new Clipboard( "[data-clipboard-text]" );
-
-clipboard.on( "success", function( e ) {
-	$( e.trigger )
-		.blur()
-		.attr( "data-hint", "Copied!" )
-		.on( "mouseout", function() {
-			$( this ).removeAttr( "data-hint" );
+	// Helper functions
+	function replace ( string, values ) {
+		$.each( values, function( key, value ) {
+			string = string.replace( new RegExp( "\\{\\{" + key + "}}", "g" ), value );
 		} );
-} );
 
-clipboard.on( "error", function( e ) {
-	$( e.trigger )
-		.blur()
-		.attr( "data-hint", "Press Ctrl+C to copy!" )
-		.on( "mouseout", function() {
-			$( this ).removeAttr( "data-hint" );
-		} );
-} );
+		return string;
+	}
+
+	clipboard = new Clipboard( "[data-clipboard-text]" );
+
+	clipboard.on( "success", function( e ) {
+		$( e.trigger )
+			.blur()
+			.attr( "data-hint", "Copied!" )
+			.on( "mouseout", function() {
+				$( this ).removeAttr( "data-hint" );
+			} );
+	} );
+
+	clipboard.on( "error", function( e ) {
+		$( e.trigger )
+			.blur()
+			.attr( "data-hint", "Press Ctrl+C to copy!" )
+			.on( "mouseout", function() {
+				$( this ).removeAttr( "data-hint" );
+			} );
+	} );
+})();
