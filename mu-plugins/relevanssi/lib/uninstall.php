0c3547a33e878125816fdf2952ae6cefef93cbf0
Paul Annekov <paul.annekov@gmail.com>
2018-02-14 10:53:01 +0100

    All: Add Relevanssi plugin, remove broken search algorithm
    Fixes #359
    Closes #361
    Closes #408

---
+++ mu-plugins/relevanssi/lib/uninstall.php
@@ -0,0 +1,32 @@
+<?php
+
+function relevanssi_clear_database_tables() {
+	global $wpdb;
+	
+	if (defined('RELEVANSSI_PREMIUM')) return;	// Relevanssi Premium exists, do not delete the tables
+	
+	wp_clear_scheduled_hook('relevanssi_truncate_cache');
+
+	$relevanssi_table = $wpdb->prefix . "relevanssi";	
+	$stopword_table = $wpdb->prefix . "relevanssi_stopwords";
+	$log_table = $wpdb->prefix . "relevanssi_log";
+	
+	if($wpdb->get_var("SHOW TABLES LIKE '$stopword_table'") == $stopword_table) {
+		$sql = "DROP TABLE $stopword_table";
+		$wpdb->query($sql);
+	}
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$relevanssi_table'") == $relevanssi_table) {
+		$sql = "DROP TABLE $relevanssi_table";
+		$wpdb->query($sql);
+	}
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$log_table'") == $log_table) {
+		$sql = "DROP TABLE $log_table";
+		$wpdb->query($sql);
+	}
+
+	echo '<div id="message" class="updated fade"><p>' . __("Data wiped clean, you can now delete the plugin.", "relevanssi") . '</p></div>';
+}
+
+?>
