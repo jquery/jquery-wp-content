7c757c14d4f3858e1a1d0aec801c51882282bf24
s-pace <sylvain.pace@algolia.com>
2020-04-15 14:31:24 +0200

    fix: prevent using DocSearch when there is no input. follows jquery/codeorigin.jquery.com#49

--- themes/jquery/footer-mobile.php
+++ themes/jquery/footer-mobile.php
@@ -9,7 +9,7 @@
 
 <?php wp_footer(); ?>
 	<!-- at the end of the BODY -->
-	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"  onload="docsearch({apiKey: '207328b0f1c18555c9021d05157dd651',
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"  onload="document.querySelector('input[name=\'s\']') && docsearch({apiKey: '207328b0f1c18555c9021d05157dd651',
 		indexName: 'jquerymobile',
 		inputSelector: 'input[name=\'s\']',
 		debug: true // Set debug to true if you want to inspect the dropdown
