caa8a5e7f90081e8d026fab89c4367cf7f4c844d
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2022-07-18 14:15:48 +0200

    Header: Remove obsolete IE conditional comments
    These are just causing issues and they only catered to IE <10.

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -1,8 +1,5 @@
 <!doctype html>
-<!--[if IE 7 ]>		 <html class="no-js ie ie7 lte7 lte8 lte9"> <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8 ]>		 <html class="no-js ie ie8 lte8 lte9"> <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 9 ]>		 <html class="no-js ie ie9 lte9"> <?php language_attributes(); ?>> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<html class="no-js" <?php language_attributes(); ?>>
 <head data-live-domain="<?php echo JQUERY_LIVE_DOMAIN; ?>">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
