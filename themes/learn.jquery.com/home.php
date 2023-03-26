153ae8d20aa622f678e528407623e2974a8fd519
Timo Tijhof <krinkle@fastmail.com>
2023-03-25 22:20:02 -0700

    learn.jquery.com: remove "Beta" ribbon
    Ref https://github.com/jquery/learn.jquery.com/pull/707.
    Ref https://github.com/jquery/learn.jquery.com/issues/569.

--- themes/learn.jquery.com/home.php
+++ themes/learn.jquery.com/home.php
@@ -4,8 +4,6 @@
 */
 get_header(); ?>
 
-<div class="beta-ribbon-wrapper"><div class="beta-ribbon"><a href="/about/#beta">Beta</a></div></div>
-
 <!-- body -->
 <div class="content-full full-width twelve columns">
 
--- themes/learn.jquery.com/page.php
+++ themes/learn.jquery.com/page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <?php the_post(); ?>
 
-<div class="beta-ribbon-wrapper"><div class="beta-ribbon"><a href="/about/#beta">Beta</a></div></div>
 <div class="content-right twelve columns">
 	<div id="content">
 		<?php get_template_part( 'content', 'page' ); ?>
