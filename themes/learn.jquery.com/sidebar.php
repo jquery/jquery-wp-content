f25da6c3a7637a13370d7b6697a459d8d47ff00f
Timo Tijhof <krinkle@fastmail.com>
2023-09-12 19:32:27 -0700

    learn.jquery.com: Fix PHP warning when empty search results
    Previously, the sidebar logic was trying to mark pages as active
    based on the last result, because that's what the $post variable
    will be set to from the main loop (have_posts/the_post) in e.g.
    the search.php or other theme file.
    
    Weird as that might be, where it goes wrong is when there is no
    $post variable, e.g. on a 404, empty category, or empty search
    results page.
    
    Fixes https://github.com/jquery/infrastructure-puppet/issues/31.

--- themes/learn.jquery.com/sidebar.php
+++ themes/learn.jquery.com/sidebar.php
@@ -12,7 +12,7 @@
 			<ul>
 				<?php $chapters = learn_chapter_listing(); ?>
 				<?php while ( $chapters->have_posts() ) : $chapters->the_post(); ?>
-					<?php $is_active = ($active_post->ID == $chapters->post->ID) || ($active_post->post_parent == $chapters->post->ID); ?>
+					<?php $is_active = $active_post && ($active_post->ID == $chapters->post->ID || $active_post->post_parent == $chapters->post->ID); ?>
 					<li <?php if ($is_active) { echo "class='active'"; } ?>>
 						<a href="<?php the_permalink(); ?>">
 							<?php if ( get_post_meta( $post->ID, "icon" ) ) : ?>
@@ -32,7 +32,7 @@
 								<ul class="sub-chapter">
 								<?php while ( $sub_chapters->have_posts() ) : $sub_chapters->the_post(); ?>
 									<?php if ( has_children( $post ) ) { ?>
-									<?php $is_active = ($active_post->ID == $sub_chapters->post->ID) || ($active_post->post_parent == $sub_chapters->post->ID); ?>
+									<?php $is_active = $active_post && ($active_post->ID == $sub_chapters->post->ID || $active_post->post_parent == $sub_chapters->post->ID); ?>
 									<li <?php if ($is_active) { echo "class='active'"; } ?>>
 										<a href="<?php the_permalink(); ?>">
 											<?php the_title(); ?>
