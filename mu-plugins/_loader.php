11ce1d6a520a0ba105cc76fa817727ce94e72e78
Timo Tijhof <krinkle@fastmail.com>
2023-09-17 19:05:55 -0700

    All: Remove relevanssi plugin
    We never enabled it on the new infra for doc sites, and it was also
    never enabled on the blogs.
    
    The default search seems good enough on WordPress 6, plus for the
    most important sites (jquery.com, jqueryui.com) we use
    Typesense/Algolia
    
    Ref https://github.com/jquery/infrastructure-puppet/issues/17.
    Ref https://github.com/jquery/infrastructure-puppet/issues/33.

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -1,6 +1,5 @@
 <?php
 
 // Enabled everywhere
-require_once __DIR__ . '/../plugins/relevanssi/relevanssi.php';
 require_once __DIR__ . '/../plugins/disable-emojis/disable-emojis.php';
 require_once __DIR__ . '/../plugins/gilded-wordpress.php';
