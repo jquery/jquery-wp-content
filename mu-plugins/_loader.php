21299acb1d5a0954d5531960fe547aa9e1a6ca53
Timo Tijhof <krinkle@fastmail.com>
2023-09-17 19:05:08 -0700

    All: Move `gilded-wordpress.php` to plugins/

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -3,3 +3,4 @@
 // Enabled everywhere
 require_once __DIR__ . '/../plugins/relevanssi/relevanssi.php';
 require_once __DIR__ . '/../plugins/disable-emojis/disable-emojis.php';
+require_once __DIR__ . '/../plugins/gilded-wordpress.php';
