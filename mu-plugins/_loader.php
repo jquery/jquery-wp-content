f8491dd9ccdacef972b791e42df0797827e4f66e
Timo Tijhof <krinkle@fastmail.com>
2023-09-17 19:02:09 -0700

    Docker: Fix typo in mu-plugins for local dev setup

--- mu-plugins/_loader.php
+++ mu-plugins/_loader.php
@@ -1,5 +1,5 @@
 <?php
 
 // Enabled everywhere
-require_once __DIR__ . '/../relevanssi/relevanssi.php';
-require_once __DIR__ . '/../disable-emojis/disable-emojis.php';
+require_once __DIR__ . '/../plugins/relevanssi/relevanssi.php';
+require_once __DIR__ . '/../plugins/disable-emojis/disable-emojis.php';
