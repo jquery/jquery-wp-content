b5ddd37fdf9c40d36b81fb6e264244b361a0c6e9
Timo Tijhof <krinkle@fastmail.com>
2023-12-17 19:00:07 +0000

    jquery: Add `subtitle` option to "page-fullwidth.php" pageTemplate
    Previously, this was limited to the "jquery.org" theme
    default page template.

--- themes/jquery/page-fullwidth.php
+++ themes/jquery/page-fullwidth.php
@@ -9,7 +9,12 @@
 
 <div class="content-full full-width twelve columns">
 	<div id="content">
-		<?php if ( !count( get_post_meta( $post->ID, "hide_title" ) ) ) : ?>
+		<?php if ( get_post_meta( $post->ID, "subtitle" ) ) : ?>
+		<div id="banner-secondary" class="large-banner entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<p><?php echo get_post_meta( $post->ID, "subtitle", true ); ?></p>
+		</div>
+		<?php elseif ( !get_post_meta( $post->ID, "hide_title" ) ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		<hr>
 		<?php endif; ?>
