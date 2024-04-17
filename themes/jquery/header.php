e5359648ed45ac389b1d185603c9c45202cf5fb5
Timo Tijhof <krinkle@fastmail.com>
2024-04-17 19:19:36 +0100

    jqueryui.com: Fixup a4e2aaa1f6 list-item bullets

--- themes/jquery/header.php
+++ themes/jquery/header.php
@@ -20,7 +20,7 @@
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/i/favicon.ico">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/lib/typesense-minibar/typesense-minibar.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/base.css?v=15">
-	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=7">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=8">
 
 	<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
