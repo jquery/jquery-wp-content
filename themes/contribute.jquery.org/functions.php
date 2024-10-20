4916ceeceeaae15e166f2f486da5fcc967fcc374
Timmy Willison <timmywil@users.noreply.github.com>
2024-10-20 15:39:19 -0400

    contribute.jquery.org: add CSP exception for vimeo player
    Ref https://github.com/jquery/infrastructure-puppet/issues/54

---
+++ themes/contribute.jquery.org/functions.php
@@ -0,0 +1,8 @@
+<?php
+ 
+// Allow loading a Vimeo video on
+// https://local.contribute.jquery.org/markup-conventions/
+add_filter( 'jq_content_security_policy', function ( $policy ) {
+	$policy[ 'frame-src' ] = "'self' player.vimeo.com";
+	return $policy;
+} );
