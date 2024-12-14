0f7a2baaa145a212ccb130770f8417b885e5d472
Timmy Willison <timmywil@users.noreply.github.com>
2024-12-14 16:08:02 -0500

    jqueryui.com: add CSP exceptions for themeroller
    Ref https://github.com/jquery/infrastructure-puppet/issues/54
    Ref gh-474

--- themes/jqueryui.com/functions.php
+++ themes/jqueryui.com/functions.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once __DIR__ . '/functions.content.php';
+
+// Allow inline scripts on https://jqueryui.com/themeroller/
+// Load styles from download.jqueryui.com on https://jqueryui.com/themeroller/
+// Load images from download.jqueryui.com on https://jqueryui.com/themeroller/
+add_filter( 'jq_content_security_policy', function ( $policy ) {
+	$policy[ 'style-src' ] = "'self' 'unsafe-inline' code.jquery.com download.jqueryui.com";
+	$policy[ 'img-src' ] = "'self' data: code.jquery.com download.jqueryui.com";
+	return $policy;
+} );
