239b9f3074936652b808b4bf0714ddea85507130
Timmy Willison <timmywil@users.noreply.github.com>
2024-12-14 17:47:56 -0500

    api.jquery.com: add CSP exception for flickr for /jQuery.getJSON
    Ref https://github.com/jquery/infrastructure-puppet/issues/54
    Closes gh-474

--- themes/api.jquery.com/functions.php
+++ themes/api.jquery.com/functions.php
@@ -1,8 +1,10 @@
 <?php
 
 // Allow inline scripts and styles in API demos
+// Allow flickr script and images on https://api.jquery.com/jQuery.getJSON/
 add_filter( 'jq_content_security_policy', function ( $policy ) {
-	$policy[ 'script-src' ] = "'self' 'unsafe-inline' code.jquery.com";
+	$policy[ 'script-src' ] = "'self' 'unsafe-inline' code.jquery.com api.flickr.com";
 	$policy[ 'style-src' ] = "'self' 'unsafe-inline' code.jquery.com";
+	$policy[ 'img-src' ] = "'self' data: code.jquery.com live.staticflickr.com";
 	return $policy;
 } );
