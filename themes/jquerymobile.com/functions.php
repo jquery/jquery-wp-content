088c14f09c6f9525a2366034a580b01aeafcaa4c
Timmy Willison <timmywil@users.noreply.github.com>
2025-03-24 12:20:28 -0400

    jquerymobile.com: add frame-src CSP exceptions for mobile download builder
    Ref https://github.com/jquery/jquerymobile.com/issues/179

--- themes/jquerymobile.com/functions.php
+++ themes/jquerymobile.com/functions.php
@@ -2,8 +2,10 @@
 
 // Allow scripts from cdnjs.cloudflare.com for the download builder
 // Allow connections to the amd builder subdomain for the download builder
+// Allow frames from amd-builder.jquerymobile.com for the download builder
 add_filter( 'jq_content_security_policy', function ( $policy ) {
 	$policy[ 'script-src' ] = "'self' code.jquery.com cdnjs.cloudflare.com";
 	$policy[ 'connect-src' ] = "'self' typesense.jquery.com *.jquerymobile.com";
+	$policy[ 'frame-src' ] = "'self' *.jquerymobile.com";
 	return $policy;
 } );
