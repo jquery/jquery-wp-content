05baff2cecca66766300de2572b1eaf499b1ba78
Timo Tijhof <krinkle@fastmail.com>
2026-01-28 02:11:35 +0000

    jquery: Remove unused function jq_other_cats

--- themes/jquery/functions.jquery.php
+++ themes/jquery/functions.jquery.php
@@ -83,24 +83,6 @@ function jq_post_category () {
 	return $current_category;
 }
 
-
-
-// For category lists on category archives: Returns other categories except the current one (redundant)
-function jq_other_cats($glue = ', ') {
-	$current_cat = single_cat_title( '', false );
-	$separator = "\n";
-	$cats = explode( $separator, get_the_category_list($separator) );
-	foreach ( $cats as $i => $str ) {
-		if ( strstr( $str, ">$current_cat<" ) || strpos( $str, "Version" ) !== false ) {
-			unset($cats[$i]);
-		}
-	}
-	if ( empty($cats) )
-		return false;
-
-	return trim(join( $glue, $cats ));
-}
-
 function jq_pages_for_category( $category ) {
 	return get_posts( array(
 		'post_type' => 'page',
